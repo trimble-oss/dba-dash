2a62f3e1a35b1f905d87e205bf10be295fc63340
David Wiseman <wiseman1982@googlemail.com>
2019-07-25 10:22:56 +0100

    Support multiple sources from commandline

--- DBAChecks/Program.cs
+++ DBAChecks/Program.cs
@@ -22,8 +22,9 @@ namespace DBAChecks
     {
         public class Options
         {
-            [Option('s', "source", Required = true, HelpText = "Connection string for Monitored SQL Instance")]
-            public string Source{ get; set; }
+            [Option('s', "source", Required = true, Separator = '|', HelpText = "Connection string for Monitored SQL Instance(s).  Use pipe character (|) to specify multiple sources.")]
+            public IEnumerable<string> Source { get; set; }
+
             [Option('d', "destination", Required = true, HelpText = "Destination Connection string for DBAChecks DB")]
             public string Destination { get; set; }
 
@@ -32,6 +33,9 @@ namespace DBAChecks
      
             [Option('p', "awsprofile", Required = false, HelpText = "AWS Profile")]
             public string AWSProfile { get; set; }
+
+            [Option('i', "interval", Required = false, Default = -1, HelpText = "Interval (mins) to repeat tests.  Default = run once.")]
+            public Int32 IntervalMins { get; set; }
         }
 
         private static AWSCredentials GetAWSCredentialsFromProfile(string profileName)
@@ -88,80 +92,104 @@ namespace DBAChecks
             Parser.Default.ParseArguments<Options>(args)
            .WithParsed<Options>(o =>
            {
-           if (o.Source.StartsWith("s3://") || o.Source.StartsWith("https://"))
-           {
-               Console.WriteLine("Import from S3");
-               var uri = new Amazon.S3.Util.AmazonS3Uri(o.Source);
-               var s3Cli = getAWSClient(o.AWSProfile, uri);
-               var resp = s3Cli.ListObjects(uri.Bucket, (uri.Key + "/DBAChecks_").Replace("//", "/"));
-                foreach(var f in resp.S3Objects)
+               while (1 == 1)
+               {
+                   DateTime collectionTime = DateTime.Now;
+                   Int32 i = 1;
+                   foreach (string s in o.Source)
                    {
-                       if (f.Key.EndsWith(".json"))
-                       {           
-                           using (GetObjectResponse response = s3Cli.GetObject(f.BucketName, f.Key))
-                           using (Stream responseStream = response.ResponseStream)
-                           using (StreamReader reader = new StreamReader(responseStream))
+                       Console.WriteLine(String.Format("Processing Source {0} of {1}", i, o.Source.Count()));
+                       if (s.StartsWith("s3://") || s.StartsWith("https://"))
+                       {
+                           Console.WriteLine("Import from S3");
+                           var uri = new Amazon.S3.Util.AmazonS3Uri(s);
+                           var s3Cli = getAWSClient(o.AWSProfile, uri);
+                           var resp = s3Cli.ListObjects(uri.Bucket, (uri.Key + "/DBAChecks_").Replace("//", "/"));
+                           foreach (var f in resp.S3Objects)
                            {
-                              string json = reader.ReadToEnd();
-                              DataSet ds = JsonConvert.DeserializeObject<DataSet>(json);
-                              importer.Update(o.Destination, ds);
-                              s3Cli.DeleteObject(f.BucketName, f.Key);
+                               if (f.Key.EndsWith(".json"))
+                               {
+                                   using (GetObjectResponse response = s3Cli.GetObject(f.BucketName, f.Key))
+                                   using (Stream responseStream = response.ResponseStream)
+                                   using (StreamReader reader = new StreamReader(responseStream))
+                                   {
+                                       string json = reader.ReadToEnd();
+                                       DataSet ds = JsonConvert.DeserializeObject<DataSet>(json);
+                                       importer.Update(o.Destination, ds);
+                                       s3Cli.DeleteObject(f.BucketName, f.Key);
+                                   }
+                                   Console.WriteLine("Imported:" + f.Key);
+                               }
                            }
-                           Console.WriteLine("Imported:" + f.Key);
                        }
-                   }    
-               }
-               else if (System.IO.Directory.Exists(o.Source))
-               {
-                   Console.WriteLine("Import from Directory");
-                   foreach(var f in Directory.EnumerateFiles(o.Source,"DBAChecks*.json", SearchOption.TopDirectoryOnly))
-                   {
-                       string json= File.ReadAllText(f);
-                       DataSet ds = JsonConvert.DeserializeObject<DataSet>(json);
-                       importer.Update(o.Destination, ds);
-                       File.Delete(f);
-                       Console.WriteLine("Imported:" + f);
-                   }
-               }
-               else
-               {
-                   var collector = new DBCollector(o.Source,o.NoWMI);
-                   collector.CollectAll();
-                   var ds = collector.Data;
-                   if (o.Destination.StartsWith("s3://") || o.Destination.StartsWith("https://"))
-                   {
+                       else if (System.IO.Directory.Exists(s))
+                       {
+                           Console.WriteLine("Import from Directory");
+                           foreach (var f in Directory.EnumerateFiles(s, "DBAChecks*.json", SearchOption.TopDirectoryOnly))
+                           {
+                               string json = File.ReadAllText(f);
+                               DataSet ds = JsonConvert.DeserializeObject<DataSet>(json);
+                               importer.Update(o.Destination, ds);
+                               File.Delete(f);
+                               Console.WriteLine("Imported:" + f);
+                           }
+                       }
+                       else
+                       {
+                           Console.WriteLine("Collect from Instance:");
+                           var collector = new DBCollector(s, o.NoWMI);
+                           collector.CollectAll();
+                           var ds = collector.Data;
+                           if (o.Destination.StartsWith("s3://") || o.Destination.StartsWith("https://"))
+                           {
+                               Console.WriteLine("Upload to S3");
+                               var uri = new Amazon.S3.Util.AmazonS3Uri(o.Destination);
+                               var s3Cli = getAWSClient(o.AWSProfile, uri);
+                               var r = new Amazon.S3.Model.PutObjectRequest();
+                               string fileName = "DBAChecks_" + DateTime.UtcNow.ToString("yyyy-MM-dd HHmmss") + Guid.NewGuid().ToString() + ".json";
+                               string filePath = Path.Combine(o.Destination, fileName);
+                               string json = JsonConvert.SerializeObject(collector.Data, Formatting.None);
+                               r.ContentBody = json;
+                               r.BucketName = uri.Bucket;
+                               r.Key = (uri.Key + "/" + fileName).Replace("//", "/");
 
-                       var uri = new Amazon.S3.Util.AmazonS3Uri(o.Destination);
-                       var s3Cli = getAWSClient(o.AWSProfile, uri); 
-                       var r = new Amazon.S3.Model.PutObjectRequest();
-                        string fileName = "DBAChecks_" + DateTime.UtcNow.ToString("yyyy-MM-dd HHmmss") + Guid.NewGuid().ToString() + ".json";
-                        string filePath = Path.Combine(o.Destination, fileName);
-                        string json = JsonConvert.SerializeObject(collector.Data, Formatting.None);
-                        r.ContentBody = json;
-                        r.BucketName = uri.Bucket;
-                        r.Key =(uri.Key + "/" + fileName).Replace("//","/");
-                          
-                        s3Cli.PutObject(r);
-                     
-                       
-     
+                               s3Cli.PutObject(r);
+
+
+
+                           }
+                           else if (System.IO.Directory.Exists(o.Destination))
+                           {
+                               Console.WriteLine("Write to folder");
+                               string fileName = "DBAChecks_" + DateTime.UtcNow.ToString("yyyy-MM-dd HHmmss") + Guid.NewGuid().ToString() + ".json";
+                               string filePath = Path.Combine(o.Destination, fileName);
+                               string json = JsonConvert.SerializeObject(collector.Data, Formatting.None);
+                               File.WriteAllText(filePath, json);
+                           }
+                           else
+                           {
+                               Console.WriteLine("Update DBAChecks DB");
+                               importer.Update(o.Destination, collector.Data);
+
+                           }
+                       }
+                       i += 1;
                    }
-                   else if (System.IO.Directory.Exists(o.Destination))
+                   if (o.IntervalMins <= 0)
                    {
-                       string fileName = "DBAChecks_" + DateTime.UtcNow.ToString("yyyy-MM-dd HHmmss") + Guid.NewGuid().ToString() + ".json";
-                       string filePath = Path.Combine(o.Destination, fileName);
-                       string json = JsonConvert.SerializeObject(collector.Data, Formatting.None);
-                       File.WriteAllText(filePath, json);
+                       break;
                    }
                    else
                    {
-                      
-                       importer.Update(o.Destination, collector.Data);
-
+                       Int32 sleepTime = ((Int32)o.IntervalMins * 60000) - Convert.ToInt32(DateTime.Now.Subtract(collectionTime).TotalMilliseconds);
+                       if (sleepTime > 0)
+                       {
+                           Console.WriteLine("Sleep For:" + sleepTime.ToString() + "ms");
+                           System.Threading.Thread.Sleep(sleepTime);
+                       }
+                       Console.WriteLine("Continue....");
                    }
                }
-
-             
         
 
            });
