5ab2dd56606d286eabee133d70c72bc68620126c
David Wiseman <wiseman1982@googlemail.com>
2019-09-09 08:50:23 +0100

    Bug fix from backup update

--- DBAChecksDB/dbo/Stored Procedures/Backups_Upd.sql
+++ DBAChecksDB/dbo/Stored Procedures/Backups_Upd.sql
@@ -8,7 +8,11 @@ DECLARE @Ref VARCHAR(30)='Backups'
 IF NOT EXISTS(SELECT 1 FROM dbo.CollectionDates WHERE SnapshotDate>=@SnapshotDate AND InstanceID = @InstanceID AND Reference=@Ref)
 BEGIN
 	MERGE dbo.Backups as T
-	USING (SELECT B.LastBackup,B.type,d.DatabaseID FROM @Backups B JOIN dbo.Databases d ON d.name = B.database_name AND d.IsActive=1) as S
+	USING (SELECT B.LastBackup,B.type,d.DatabaseID 
+		FROM @Backups B 
+		JOIN dbo.Databases d ON d.name = B.database_name
+		AND d.IsActive=1
+		AND D.InstanceID=@InstanceID) as S
 		ON S.DatabaseID = T.DatabaseID AND S.type=T.type
 	WHEN MATCHED THEN
 	UPDATE SET T.LastBackup = S.LastBackup
