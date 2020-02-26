9a4931ebc366610fe0d739ea668209dad4bebcc2
David Wiseman <developer@wisesoft.co.uk>
2020-02-26 09:15:56 +0000

    Fixed issue with DB config change tracking.  Added log shipping summary view

--- DBAChecksDB/dbo/Stored Procedures/DBConfig_Upd.sql
+++ DBAChecksDB/dbo/Stored Procedures/DBConfig_Upd.sql
@@ -23,38 +23,19 @@ BEGIN
 	WHERE d.InstanceID = @InstanceID
 	AND NOT EXISTS(SELECT 1 FROM dbo.DBConfig c WHERE c.DatabaseID = d.DatabaseID AND c.configuration_id = t.configuration_id);
 
-	WITH T AS (
-		SELECT c.DatabaseID,
-			   c.configuration_id,
-			   c.value,
-			   c.value_for_secondary ,
-			   c.ValidFrom
-		FROM dbo.DBConfig c
-		JOIN dbo.Databases d ON d.database_id = c.DatabaseID
-		WHERE d.InstanceID = @InstanceID
-		AND d.IsActive=1
-	)
-	MERGE T USING(
-		SELECT d.DatabaseID,
-				c.configuration_id,
-				c.name,
-				c.value,
-				c.value_for_secondary
-		FROM @DBConfig c
-		JOIN dbo.Databases d ON d.database_id = c.database_id
-		WHERE d.IsActive=1
-		) AS S
-		ON S.configuration_id = T.configuration_id AND s.DatabaseID = T.DatabaseID
-	WHEN MATCHED AND NOT ( 
-						(t.value = S.value OR (t.value IS NULL AND S.value IS NULL))
-					AND (t.value_for_secondary = S.value_for_secondary OR (t.value_for_secondary IS NULL AND S.value_for_secondary IS NULL))
-					) THEN
-	UPDATE 
-	SET T.value = S.value,
-	T.value_for_secondary = S.value_for_secondary
-	WHEN NOT MATCHED BY SOURCE THEN 
-	DELETE
-	OUTPUT DELETED.DatabaseID,DELETED.configuration_id,DELETED.value,DELETED.value_for_secondary,INSERTED.value,INSERTED.value_for_secondary,DELETED.ValidFrom,@SnapshotDate INTO dbo.DBConfigHistory(DatabaseID,configuration_id,value,value_for_secondary,new_value,new_value_for_secondary,ValidFrom,ValidTo);
+	UPDATE c
+	SET C.value = cfg.value,
+		C.value_for_secondary = cfg.value_for_secondary
+	OUTPUT DELETED.DatabaseID,DELETED.configuration_id,DELETED.value,DELETED.value_for_secondary,INSERTED.value,INSERTED.value_for_secondary,DELETED.ValidFrom,@SnapshotDate INTO dbo.DBConfigHistory(DatabaseID,configuration_id,value,value_for_secondary,new_value,new_value_for_secondary,ValidFrom,ValidTo)
+	FROM dbo.DBConfig c
+	JOIN dbo.Databases d ON d.DatabaseID = c.DatabaseID
+	JOIN @DBConfig cfg ON c.configuration_id = cfg.configuration_id AND cfg.database_id = d.database_id
+	WHERE d.InstanceID = @InstanceID
+	AND d.IsActive=1
+	AND (EXISTS(SELECT c.value EXCEPT SELECT cfg.value)
+		OR EXISTS(SELECT c.value_for_secondary EXCEPT SELECT cfg.value_for_secondary)
+		);
+
 
 	EXEC dbo.CollectionDates_Upd @InstanceID = @InstanceID,  
 										 @Reference = @Ref,
---
+++ DBAChecksDB/dbo/Views/LogShippingStatusSummary.sql
@@ -0,0 +1,18 @@
+﻿CREATE VIEW LogShippingStatusSummary 
+AS
+SELECT InstanceID,
+       Instance,
+	   MIN(LSS.Status) AS Status,
+	   CASE MIN(LSS.Status) WHEN 1 THEN 'Critical' WHEN 2 THEN 'Warning' WHEN 3 THEN 'N/A' WHEN 4 THEN 'OK' END AS StatusDescription,
+	   COUNT(*) AS LogShippedDBCount,
+	   SUM(CASE WHEN Status=2 THEN 1 ELSE 0 END) AS WarningCount,
+	   SUM(CASE WHEN Status=1 THEN 1 ELSE 0 END) AS CriticalCount,
+	   MAX(LSS.TotalTimeBehind) MaxTotalTimeBehind,
+	   MAX(LSS.LatencyOfLast) MaxLatencyOfLast,
+	   MAX(LSS.TimeSinceLast) AS TimeSinceLast,
+	   MAX(LSS.SnapshotAge) AS SnapshotAge,
+	   MIN(LSS.backup_start_date) MinDateOfLastBackupRestored,
+	   MIN(LSS.restore_date) MinLastRestoreCompleted
+FROM dbo.LogShippingStatus LSS
+WHERE LSS.Status<> 3
+GROUP BY LSS.InstanceID,LSS.Instance
