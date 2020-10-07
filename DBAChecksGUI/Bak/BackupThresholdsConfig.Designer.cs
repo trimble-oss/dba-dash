d553e5f125f63c559fc24c831c5007be376b87a5
David Wiseman <>
2020-10-07 10:46:58 +0100

    Backups
    Changed name of folder as the name caused it to be excluded from source control.  Also minor layout changes

---
+++ DBAChecksGUI/Bak/BackupThresholdsConfig.Designer.cs
@@ -0,0 +1,401 @@
+﻿namespace DBAChecksGUI.Backups
+{
+    partial class BackupThresholdsConfig
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chkUseFG = new System.Windows.Forms.CheckBox();
+            this.chkUsePartial = new System.Windows.Forms.CheckBox();
+            this.chkBackupInherit = new System.Windows.Forms.CheckBox();
+            this.pnlBackupThresholds = new System.Windows.Forms.Panel();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.numLogWarning = new System.Windows.Forms.NumericUpDown();
+            this.numLogCritical = new System.Windows.Forms.NumericUpDown();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.numDiffWarning = new System.Windows.Forms.NumericUpDown();
+            this.numDiffCritical = new System.Windows.Forms.NumericUpDown();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.chkLog = new System.Windows.Forms.CheckBox();
+            this.chkDiff = new System.Windows.Forms.CheckBox();
+            this.chkFull = new System.Windows.Forms.CheckBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.numFullWarning = new System.Windows.Forms.NumericUpDown();
+            this.numFullCritical = new System.Windows.Forms.NumericUpDown();
+            this.bttnUpdate = new System.Windows.Forms.Button();
+            this.pnlBackupThresholds.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numLogWarning)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numLogCritical)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDiffWarning)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDiffCritical)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numFullWarning)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numFullCritical)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chkUseFG
+            // 
+            this.chkUseFG.AutoSize = true;
+            this.chkUseFG.Location = new System.Drawing.Point(296, 147);
+            this.chkUseFG.Name = "chkUseFG";
+            this.chkUseFG.Size = new System.Drawing.Size(176, 21);
+            this.chkUseFG.TabIndex = 44;
+            this.chkUseFG.Text = "Use Filegroup Backups";
+            this.chkUseFG.UseVisualStyleBackColor = true;
+            // 
+            // chkUsePartial
+            // 
+            this.chkUsePartial.AutoSize = true;
+            this.chkUsePartial.Location = new System.Drawing.Point(122, 147);
+            this.chkUsePartial.Name = "chkUsePartial";
+            this.chkUsePartial.Size = new System.Drawing.Size(157, 21);
+            this.chkUsePartial.TabIndex = 43;
+            this.chkUsePartial.Text = "Use Partial Backups";
+            this.chkUsePartial.UseVisualStyleBackColor = true;
+            // 
+            // chkBackupInherit
+            // 
+            this.chkBackupInherit.AutoSize = true;
+            this.chkBackupInherit.Location = new System.Drawing.Point(24, 14);
+            this.chkBackupInherit.Name = "chkBackupInherit";
+            this.chkBackupInherit.Size = new System.Drawing.Size(69, 21);
+            this.chkBackupInherit.TabIndex = 42;
+            this.chkBackupInherit.Text = "Inherit";
+            this.chkBackupInherit.UseVisualStyleBackColor = true;
+            this.chkBackupInherit.CheckedChanged += new System.EventHandler(this.chkBackupInherit_CheckedChanged);
+            // 
+            // pnlBackupThresholds
+            // 
+            this.pnlBackupThresholds.Controls.Add(this.label15);
+            this.pnlBackupThresholds.Controls.Add(this.chkUseFG);
+            this.pnlBackupThresholds.Controls.Add(this.label16);
+            this.pnlBackupThresholds.Controls.Add(this.chkUsePartial);
+            this.pnlBackupThresholds.Controls.Add(this.numLogWarning);
+            this.pnlBackupThresholds.Controls.Add(this.numLogCritical);
+            this.pnlBackupThresholds.Controls.Add(this.label13);
+            this.pnlBackupThresholds.Controls.Add(this.label14);
+            this.pnlBackupThresholds.Controls.Add(this.numDiffWarning);
+            this.pnlBackupThresholds.Controls.Add(this.numDiffCritical);
+            this.pnlBackupThresholds.Controls.Add(this.label10);
+            this.pnlBackupThresholds.Controls.Add(this.label8);
+            this.pnlBackupThresholds.Controls.Add(this.chkLog);
+            this.pnlBackupThresholds.Controls.Add(this.chkDiff);
+            this.pnlBackupThresholds.Controls.Add(this.chkFull);
+            this.pnlBackupThresholds.Controls.Add(this.label7);
+            this.pnlBackupThresholds.Controls.Add(this.label9);
+            this.pnlBackupThresholds.Controls.Add(this.numFullWarning);
+            this.pnlBackupThresholds.Controls.Add(this.numFullCritical);
+            this.pnlBackupThresholds.Location = new System.Drawing.Point(13, 42);
+            this.pnlBackupThresholds.Margin = new System.Windows.Forms.Padding(4);
+            this.pnlBackupThresholds.Name = "pnlBackupThresholds";
+            this.pnlBackupThresholds.Size = new System.Drawing.Size(509, 193);
+            this.pnlBackupThresholds.TabIndex = 41;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(229, 101);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(37, 17);
+            this.label15.TabIndex = 53;
+            this.label15.Text = "mins";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(404, 101);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(37, 17);
+            this.label16.TabIndex = 52;
+            this.label16.Text = "mins";
+            // 
+            // numLogWarning
+            // 
+            this.numLogWarning.Location = new System.Drawing.Point(122, 100);
+            this.numLogWarning.Margin = new System.Windows.Forms.Padding(4);
+            this.numLogWarning.Maximum = new decimal(new int[] {
+            2147483647,
+            0,
+            0,
+            0});
+            this.numLogWarning.Name = "numLogWarning";
+            this.numLogWarning.Size = new System.Drawing.Size(100, 22);
+            this.numLogWarning.TabIndex = 50;
+            this.numLogWarning.Value = new decimal(new int[] {
+            720,
+            0,
+            0,
+            0});
+            // 
+            // numLogCritical
+            // 
+            this.numLogCritical.Location = new System.Drawing.Point(296, 100);
+            this.numLogCritical.Margin = new System.Windows.Forms.Padding(4);
+            this.numLogCritical.Maximum = new decimal(new int[] {
+            2147483647,
+            0,
+            0,
+            0});
+            this.numLogCritical.Name = "numLogCritical";
+            this.numLogCritical.Size = new System.Drawing.Size(100, 22);
+            this.numLogCritical.TabIndex = 51;
+            this.numLogCritical.Value = new decimal(new int[] {
+            1440,
+            0,
+            0,
+            0});
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(229, 71);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(37, 17);
+            this.label13.TabIndex = 49;
+            this.label13.Text = "mins";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(404, 71);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(37, 17);
+            this.label14.TabIndex = 48;
+            this.label14.Text = "mins";
+            // 
+            // numDiffWarning
+            // 
+            this.numDiffWarning.Enabled = false;
+            this.numDiffWarning.Location = new System.Drawing.Point(122, 70);
+            this.numDiffWarning.Margin = new System.Windows.Forms.Padding(4);
+            this.numDiffWarning.Maximum = new decimal(new int[] {
+            2147483647,
+            0,
+            0,
+            0});
+            this.numDiffWarning.Name = "numDiffWarning";
+            this.numDiffWarning.Size = new System.Drawing.Size(100, 22);
+            this.numDiffWarning.TabIndex = 46;
+            this.numDiffWarning.Value = new decimal(new int[] {
+            1440,
+            0,
+            0,
+            0});
+            // 
+            // numDiffCritical
+            // 
+            this.numDiffCritical.Enabled = false;
+            this.numDiffCritical.Location = new System.Drawing.Point(296, 70);
+            this.numDiffCritical.Margin = new System.Windows.Forms.Padding(4);
+            this.numDiffCritical.Maximum = new decimal(new int[] {
+            2147483647,
+            0,
+            0,
+            0});
+            this.numDiffCritical.Name = "numDiffCritical";
+            this.numDiffCritical.Size = new System.Drawing.Size(100, 22);
+            this.numDiffCritical.TabIndex = 47;
+            this.numDiffCritical.Value = new decimal(new int[] {
+            2880,
+            0,
+            0,
+            0});
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(229, 41);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(37, 17);
+            this.label10.TabIndex = 45;
+            this.label10.Text = "mins";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(404, 41);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(37, 17);
+            this.label8.TabIndex = 44;
+            this.label8.Text = "mins";
+            // 
+            // chkLog
+            // 
+            this.chkLog.AutoSize = true;
+            this.chkLog.Checked = true;
+            this.chkLog.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkLog.Location = new System.Drawing.Point(14, 101);
+            this.chkLog.Name = "chkLog";
+            this.chkLog.Size = new System.Drawing.Size(54, 21);
+            this.chkLog.TabIndex = 43;
+            this.chkLog.Text = "Log";
+            this.chkLog.UseVisualStyleBackColor = true;
+            this.chkLog.CheckedChanged += new System.EventHandler(this.chkLog_CheckedChanged);
+            // 
+            // chkDiff
+            // 
+            this.chkDiff.AutoSize = true;
+            this.chkDiff.Location = new System.Drawing.Point(13, 71);
+            this.chkDiff.Name = "chkDiff";
+            this.chkDiff.Size = new System.Drawing.Size(51, 21);
+            this.chkDiff.TabIndex = 42;
+            this.chkDiff.Text = "Diff";
+            this.chkDiff.UseVisualStyleBackColor = true;
+            this.chkDiff.CheckedChanged += new System.EventHandler(this.chkDiff_CheckedChanged);
+            // 
+            // chkFull
+            // 
+            this.chkFull.AutoSize = true;
+            this.chkFull.Checked = true;
+            this.chkFull.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkFull.Location = new System.Drawing.Point(13, 41);
+            this.chkFull.Name = "chkFull";
+            this.chkFull.Size = new System.Drawing.Size(52, 21);
+            this.chkFull.TabIndex = 41;
+            this.chkFull.Text = "Full";
+            this.chkFull.UseVisualStyleBackColor = true;
+            this.chkFull.CheckedChanged += new System.EventHandler(this.chkFull_CheckedChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(293, 10);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(54, 17);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Critical:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(119, 10);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(65, 17);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Warning:";
+            // 
+            // numFullWarning
+            // 
+            this.numFullWarning.Location = new System.Drawing.Point(122, 40);
+            this.numFullWarning.Margin = new System.Windows.Forms.Padding(4);
+            this.numFullWarning.Maximum = new decimal(new int[] {
+            2147483647,
+            0,
+            0,
+            0});
+            this.numFullWarning.Name = "numFullWarning";
+            this.numFullWarning.Size = new System.Drawing.Size(100, 22);
+            this.numFullWarning.TabIndex = 22;
+            this.numFullWarning.Value = new decimal(new int[] {
+            10080,
+            0,
+            0,
+            0});
+            // 
+            // numFullCritical
+            // 
+            this.numFullCritical.Location = new System.Drawing.Point(296, 40);
+            this.numFullCritical.Margin = new System.Windows.Forms.Padding(4);
+            this.numFullCritical.Maximum = new decimal(new int[] {
+            2147483647,
+            0,
+            0,
+            0});
+            this.numFullCritical.Name = "numFullCritical";
+            this.numFullCritical.Size = new System.Drawing.Size(100, 22);
+            this.numFullCritical.TabIndex = 23;
+            this.numFullCritical.Value = new decimal(new int[] {
+            14400,
+            0,
+            0,
+            0});
+            // 
+            // bttnUpdate
+            // 
+            this.bttnUpdate.Location = new System.Drawing.Point(447, 242);
+            this.bttnUpdate.Name = "bttnUpdate";
+            this.bttnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.bttnUpdate.TabIndex = 45;
+            this.bttnUpdate.Text = "Update";
+            this.bttnUpdate.UseVisualStyleBackColor = true;
+            this.bttnUpdate.Click += new System.EventHandler(this.bttnUpdate_Click);
+            // 
+            // BackupThresholdsConfig
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 275);
+            this.Controls.Add(this.bttnUpdate);
+            this.Controls.Add(this.chkBackupInherit);
+            this.Controls.Add(this.pnlBackupThresholds);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "BackupThresholdsConfig";
+            this.Text = "Backup Thresholds";
+            this.Load += new System.EventHandler(this.BackupThresholds_Load);
+            this.pnlBackupThresholds.ResumeLayout(false);
+            this.pnlBackupThresholds.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numLogWarning)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numLogCritical)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDiffWarning)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDiffCritical)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numFullWarning)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numFullCritical)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chkUseFG;
+        private System.Windows.Forms.CheckBox chkUsePartial;
+        private System.Windows.Forms.CheckBox chkBackupInherit;
+        private System.Windows.Forms.Panel pnlBackupThresholds;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.NumericUpDown numLogWarning;
+        private System.Windows.Forms.NumericUpDown numLogCritical;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.NumericUpDown numDiffWarning;
+        private System.Windows.Forms.NumericUpDown numDiffCritical;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.CheckBox chkLog;
+        private System.Windows.Forms.CheckBox chkDiff;
+        private System.Windows.Forms.CheckBox chkFull;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.NumericUpDown numFullWarning;
+        private System.Windows.Forms.NumericUpDown numFullCritical;
+        private System.Windows.Forms.Button bttnUpdate;
+    }
+}
---
+++ DBAChecksGUI/Bak/BackupsControl.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DBAChecksGUI.Backups
+{
+    public partial class BackupsControl : UserControl
+    {
+
+        public bool IncludeCritical
+        {
+            get
+            {
+                return criticalToolStripMenuItem.Checked;
+            }
+            set
+            {
+                criticalToolStripMenuItem.Checked = value;
+            }
+        }
+            
+        public bool IncludeWarning
+        {
+            get
+            {
+                return warningToolStripMenuItem.Checked;
+            }
+            set
+            {
+                warningToolStripMenuItem.Checked = value;
+            }
+        }
+        public bool IncludeNA
+        {
+            get
+            {
+                return undefinedToolStripMenuItem.Checked;
+            }
+            set
+            {
+                undefinedToolStripMenuItem.Checked = value;
+            }
+        }
+        public bool IncludeOK
+        {
+            get
+            {
+                return OKToolStripMenuItem.Checked;
+            }
+            set
+            {
+                OKToolStripMenuItem.Checked = value;
+            }
+        }
+
+        public List<Int32> InstanceIDs { get; set; }
+        public List<Int32> DatabaseID { get; set; }
+        public string ConnectionString;
+
+
+        public void RefreshBackups()
+        {
+            UseWaitCursor = true;
+            if (ConnectionString != null)
+            {
+                configureInstanceThresholdsToolStripMenuItem.Enabled = (InstanceIDs.Count == 1);
+                SqlConnection cn = new SqlConnection(ConnectionString);
+                using (cn)
+                {
+                    cn.Open();
+                    SqlCommand cmd = new SqlCommand("dbo.Backups_Get", cn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("InstanceIDs", string.Join(",", InstanceIDs));
+                    cmd.Parameters.AddWithValue("IncludeCritical", IncludeCritical);
+                    cmd.Parameters.AddWithValue("IncludeWarning", IncludeWarning);
+                    cmd.Parameters.AddWithValue("IncludeNA", IncludeNA);
+                    cmd.Parameters.AddWithValue("IncludeOK", IncludeOK);
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dtBackups = new DataTable();
+                    da.Fill(dtBackups);
+                    dgvBackups.AutoGenerateColumns = false;
+                    dgvBackups.DataSource = new DataView(dtBackups);
+                }
+                
+            }
+            UseWaitCursor = false;
+        }
+
+        public BackupsControl()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void dgvBackups_RowsAdded(object sender, DataGridViewRowsAddedEventArgs e)
+        {
+            for (Int32 idx = e.RowIndex; idx < e.RowIndex + e.RowCount; idx += 1)
+            {
+                var row = (DataRowView)dgvBackups.Rows[idx].DataBoundItem;
+                var fullBackupStatus = (DBAChecksStatus.DBAChecksStatusEnum)row["FullBackupStatus"];
+                var diffBackupStatus = (DBAChecksStatus.DBAChecksStatusEnum)row["DiffBackupStatus"];
+                var logBackupStatus = (DBAChecksStatus.DBAChecksStatusEnum)row["LogBackupStatus"];
+                var snapshotStatus = (DBAChecksStatus.DBAChecksStatusEnum)row["SnapshotAgeStatus"];
+                dgvBackups.Rows[idx].Cells["LastFull"].Style.BackColor = DBAChecksStatus.GetStatusColour(fullBackupStatus);
+                dgvBackups.Rows[idx].Cells["LastDiff"].Style.BackColor = DBAChecksStatus.GetStatusColour(diffBackupStatus);
+                dgvBackups.Rows[idx].Cells["LastLog"].Style.BackColor = DBAChecksStatus.GetStatusColour(logBackupStatus);
+                if ((bool)row["ConsiderPartialBackups"])
+                {
+                    dgvBackups.Rows[idx].Cells["LastPartial"].Style.BackColor = dgvBackups.Rows[idx].Cells["LastFull"].Style.BackColor;
+                    dgvBackups.Rows[idx].Cells["LastPartialDiff"].Style.BackColor = dgvBackups.Rows[idx].Cells["LastDiff"].Style.BackColor;
+                }
+                if ((bool)row["ConsiderFGBackups"])
+                {
+                    dgvBackups.Rows[idx].Cells["LastFG"].Style.BackColor = dgvBackups.Rows[idx].Cells["LastFull"].Style.BackColor;
+                    dgvBackups.Rows[idx].Cells["LastFGDiff"].Style.BackColor = dgvBackups.Rows[idx].Cells["LastDiff"].Style.BackColor;
+                }
+                dgvBackups.Rows[idx].Cells["SnapshotAge"].Style.BackColor = DBAChecksStatus.GetStatusColour(snapshotStatus);
+                if ((string)row["ThresholdsConfiguredLevel"] == "Database")
+                {
+                    dgvBackups.Rows[idx].Cells["Configure"].Style.Font = new Font(dgvBackups.Font, FontStyle.Bold);
+                }
+                else
+                {
+                    dgvBackups.Rows[idx].Cells["Configure"].Style.Font = new Font(dgvBackups.Font, FontStyle.Regular);
+                }
+            }
+
+        }
+
+        private void dgvBackups_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvBackups.Columns[e.ColumnIndex].HeaderText == "Configure")
+            {
+                var row = (DataRowView)dgvBackups.Rows[e.RowIndex].DataBoundItem;
+                ConfigureThresholds((Int32)row["InstanceID"], (Int32)row["DatabaseID"]);
+            }
+        }
+
+        private void ConfigureThresholds(Int32 InstanceID, Int32 DatabaseID)
+        {
+            var frm = new BackupThresholdsConfig();
+            frm.InstanceID = InstanceID;
+            frm.DatabaseID = DatabaseID;
+            frm.ConnectionString = ConnectionString;
+            frm.ShowDialog();
+            if (frm.DialogResult == DialogResult.OK)
+            {
+                RefreshBackups();
+            }
+
+        }
+
+        private void configureRootThresholdsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ConfigureThresholds(-1, -1);
+        }
+
+        private void configureInstanceThresholdsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (InstanceIDs.Count == 1)
+            {
+                ConfigureThresholds(InstanceIDs[0], -1);
+            }
+        }
+
+        private void criticalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            RefreshBackups();
+
+        }
+
+        private void warningToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            RefreshBackups();
+        }
+
+        private void undefinedToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            RefreshBackups();
+        }
+
+        private void OKToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            RefreshBackups();
+        }
+
+        private void tsRefresh_Click(object sender, EventArgs e)
+        {
+            RefreshBackups();
+        }
+
+        private void tsCopy_Click(object sender, EventArgs e)
+        {
+            Configure.Visible = false;
+            Common.CopyDataGridViewToClipboard(dgvBackups);
+            Configure.Visible = true;
+        }
+    }
+}
