08294e430631f0949c7ec7fd0af8186ebf69a1eb
David Wiseman <>
2020-11-20 10:29:54 +0000

    Update Tags_Get.sql
    Filter out tags that are not associated with any active instances

--- DBAChecksDB/dbo/Stored Procedures/Tags_Get.sql
+++ DBAChecksDB/dbo/Stored Procedures/Tags_Get.sql
@@ -1,5 +1,11 @@
-﻿CREATE PROC Tags_Get
+﻿CREATE PROC dbo.Tags_Get
 AS
 SELECT TagID,TagName,TagValue 
-FROM dbo.Tags
+FROM dbo.Tags T 
+WHERE EXISTS(SELECT 1 
+			FROM dbo.InstanceTags IT 
+			JOIN dbo.Instances I ON IT.Instance = I.Instance
+			WHERE IT.TagID = T.TagID
+			AND I.IsActive=1
+			)
 ORDER BY TagName,TagValue
