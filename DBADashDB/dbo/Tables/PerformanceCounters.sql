1ecda772b26852ef71dfcbdadf2da1b04c794147
David Wiseman <>
2021-01-21 21:02:27 +0000

    Update data compression settings

--- DBADashDB/dbo/Tables/PerformanceCounters.sql
+++ DBADashDB/dbo/Tables/PerformanceCounters.sql
@@ -3,7 +3,7 @@
     [CounterID]    INT             NOT NULL,
     [SnapshotDate] DATETIME2 (2)   NOT NULL,
     [Value]        DECIMAL (28, 9) NOT NULL,
-    CONSTRAINT [PK_PerformanceCounters] PRIMARY KEY CLUSTERED ([InstanceID] ASC, [CounterID] ASC, [SnapshotDate] ASC) ON [PS_PerformanceCounters] ([SnapshotDate]),
+    CONSTRAINT [PK_PerformanceCounters] PRIMARY KEY CLUSTERED ([InstanceID] ASC, [CounterID] ASC, [SnapshotDate] ASC) WITH(DATA_COMPRESSION=PAGE) ON [PS_PerformanceCounters] ([SnapshotDate]),
     CONSTRAINT [FK_PerformanceCounters_Counters] FOREIGN KEY ([CounterID]) REFERENCES [dbo].[Counters] ([CounterID]),
     CONSTRAINT [FK_PerformanceCounters_Instances] FOREIGN KEY ([InstanceID]) REFERENCES [dbo].[Instances] ([InstanceID])
 ) ON [PS_PerformanceCounters] ([SnapshotDate]);
