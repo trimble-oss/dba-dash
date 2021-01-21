3a740e017acaac8478539fed06e599236dcd2fb3
David Wiseman <>
2021-01-21 08:02:18 +0000

    Fix truncation issue
    AzureDBElasticPoolResourceStats was truncated

--- DBADashDB/dbo/Tables/CollectionDates.sql
+++ DBADashDB/dbo/Tables/CollectionDates.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[CollectionDates] (
     [InstanceID]   INT           NOT NULL,
-    [Reference]    VARCHAR (30)  NOT NULL,
+    [Reference]    VARCHAR (100)  NOT NULL,
     [SnapshotDate] DATETIME2 (2) NOT NULL,
     CONSTRAINT [PK_CollectionDates] PRIMARY KEY CLUSTERED ([InstanceID] ASC, [Reference] ASC),
     CONSTRAINT [FK_CollectionDates] FOREIGN KEY ([InstanceID]) REFERENCES [dbo].[Instances] ([InstanceID])
