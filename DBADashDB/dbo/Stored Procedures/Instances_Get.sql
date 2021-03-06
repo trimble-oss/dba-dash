86d002e7eaf53fb2f72c0d5f3c3d2608f4282350
David Wiseman <>
2021-03-06 19:56:59 +0000

    Added IsActive filter
    Added IsActive filter to Instances_Get

--- DBADashDB/dbo/Stored Procedures/Instances_Get.sql
+++ DBADashDB/dbo/Stored Procedures/Instances_Get.sql
@@ -2,6 +2,6 @@
 AS
 SELECT  I.InstanceID,I.ConnectionID,I.Instance,CASE WHEN I.EditionID = 1674378470 THEN CAST(1 AS BIT) ELSE CAST(0 AS BIT) END AS IsAzure,D.DatabaseID AS AzureDatabaseID,D.name AS AzureDBName
 FROM dbo.InstancesMatchingTags(@TagIDs) I
-LEFT JOIN dbo.Databases D ON D.InstanceID = I.InstanceID AND I.EditionID = 1674378470 
+LEFT JOIN dbo.Databases D ON D.InstanceID = I.InstanceID AND I.EditionID = 1674378470 AND D.IsActive=1
 WHERE I.IsActive=1
 ORDER BY I.Instance,D.name
