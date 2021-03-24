b77710026201bea3f79a817c962a53c839651d6f
David Wiseman <>
2021-03-24 10:24:36 +0000

    Sort pool history by change date

--- DBADashDB/dbo/Stored Procedures/AzureDBElasticPoolHistory_Get.sql
+++ DBADashDB/dbo/Stored Procedures/AzureDBElasticPoolHistory_Get.sql
@@ -1,5 +1,4 @@
-﻿
-CREATE PROC dbo.AzureDBElasticPoolHistory_Get(
+﻿CREATE PROC dbo.AzureDBElasticPoolHistory_Get(
 	@InstanceIDs VARCHAR(MAX)
 )
 AS
@@ -16,3 +15,4 @@ FROM dbo.AzureDBElasticPoolHistory PH
 JOIN dbo.AzureDBElasticPool P ON P.PoolID = PH.PoolID
 JOIN dbo.Instances I ON P.InstanceID = I.InstanceID
 WHERE EXISTS(SELECT 1 FROM STRING_SPLIT(@InstanceIDs,',') ss WHERE ss.value = I.InstanceID)
+ORDER BY PH.ValidTo DESC
