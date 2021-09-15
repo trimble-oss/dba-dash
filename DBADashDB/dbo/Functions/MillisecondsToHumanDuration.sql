296f927d9bb970147888116db473c75a580d8a61
David Wiseman <wiseman1982@googlemail.com>
2021-09-15 19:50:53 +0100

    Improve HumanDuration functions
    Handle negative values and values over 100 days.

--- DBADashDB/dbo/Functions/MillisecondsToHumanDuration.sql
+++ DBADashDB/dbo/Functions/MillisecondsToHumanDuration.sql
@@ -3,10 +3,13 @@
 )
 RETURNS TABLE
 AS
-RETURN SELECT CONCAT(
-                        RIGHT('00' + CAST(ISNULL((@ms / 3600000 / 24), 0) AS VARCHAR(2)), 2), ' ',
-                        RIGHT('00' + CAST(ISNULL(@ms / 3600000 % 24, 0) AS VARCHAR(2)), 2), ':',
-                        RIGHT('00' + CAST(ISNULL(@ms / 60000 % 60, 0) AS VARCHAR(2)), 2), ':',
-                        RIGHT('00' + CAST(ISNULL(@ms / 1000 % 3600 % 60, 0) AS VARCHAR(2)), 2) + '.',
-						RIGHT('000' + CAST(ISNULL(@ms % 1000, 0) AS VARCHAR(3)), 3) 
-                    ) AS HumanDuration;
+RETURN SELECT CASE WHEN ABS(@ms)>=8640000000 THEN CONCAT(@ms/86400000,' Days') ELSE
+				CONCAT(
+						CASE WHEN @ms<0 THEN '-' ELSE '' END,
+                        RIGHT('00' + CAST(ISNULL((ABS(@ms) / 3600000 / 24), 0) AS VARCHAR(2)), 2), ' ',
+                        RIGHT('00' + CAST(ISNULL(ABS(@ms) / 3600000 % 24, 0) AS VARCHAR(2)), 2), ':',
+                        RIGHT('00' + CAST(ISNULL(ABS(@ms) / 60000 % 60, 0) AS VARCHAR(2)), 2), ':',
+                        RIGHT('00' + CAST(ISNULL(ABS(@ms) / 1000 % 3600 % 60, 0) AS VARCHAR(2)), 2) + '.',
+						RIGHT('000' + CAST(ISNULL(ABS(@ms) % 1000, 0) AS VARCHAR(3)), 3) 
+                    )
+				END AS HumanDuration;
--- DBADashDB/dbo/Functions/SecondsToHumanDuration.sql
+++ DBADashDB/dbo/Functions/SecondsToHumanDuration.sql
@@ -3,9 +3,12 @@
 )
 RETURNS TABLE
 AS
-RETURN SELECT CONCAT(
-                        RIGHT('00' + CAST(ISNULL((@Seconds / 3600 / 24), 0) AS VARCHAR(2)), 2), ' ',
-                        RIGHT('00' + CAST(ISNULL(@Seconds / 3600 % 24, 0) AS VARCHAR(2)), 2), ':',
-                        RIGHT('00' + CAST(ISNULL(@Seconds / 60 % 60, 0) AS VARCHAR(2)), 2), ':',
-                        RIGHT('00' + CAST(ISNULL(@Seconds % 3600 % 60, 0) AS VARCHAR(2)), 2)
-                    ) AS HumanDuration;
+RETURN SELECT CASE WHEN ABS(@Seconds)>=8640000 THEN CONCAT(@Seconds/86400,' Days') ELSE
+				CONCAT(
+						CASE WHEN @Seconds<0 THEN '-' ELSE '' END,
+                        RIGHT('00' + CAST(ISNULL((ABS(@Seconds) / 3600 / 24), 0) AS VARCHAR(2)), 2), ' ',
+                        RIGHT('00' + CAST(ISNULL(ABS(@Seconds) / 3600 % 24, 0) AS VARCHAR(2)), 2), ':',
+                        RIGHT('00' + CAST(ISNULL(ABS(@Seconds) / 60 % 60, 0) AS VARCHAR(2)), 2), ':',
+                        RIGHT('00' + CAST(ISNULL(ABS(@Seconds) % 3600 % 60, 0) AS VARCHAR(2)), 2)
+                    )
+				END AS HumanDuration;
