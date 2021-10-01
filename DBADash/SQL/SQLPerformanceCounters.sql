488ea33b1332c2282cb59c417abf803da297a35e
David Wiseman <wiseman1982@googlemail.com>
2021-10-01 12:05:46 +0100

    Add performance counters

--- DBADash/SQL/SQLPerformanceCounters.sql
+++ DBADash/SQL/SQLPerformanceCounters.sql
@@ -7,6 +7,57 @@ INSERT INTO @counters(object_name,counter_name,instance_name)
 SELECT ctrs.c.value('@object_name','NCHAR(128)'),ctrs.c.value('@counter_name','NCHAR(128)') ,ctrs.c.value('@instance_name','NCHAR(128)') 
 FROM @CountersXML.nodes('Counters/Counter') ctrs(c)
 
+DECLARE @AvailablePhysicalMemory DECIMAL(28,9)
+DECLARE @MemoryHighSignalState DECIMAL(28,9)
+DECLARE @MemoryLowSignalState DECIMAL(28,9)
+DECLARE @CountOfNodesWithThreadResourcesLow DECIMAL(28,9)
+
+IF EXISTS(SELECT * FROM @counters WHERE object_name='sys.dm_os_sys_memory')
+BEGIN
+	SELECT @AvailablePhysicalMemory= available_physical_memory_kb,
+		@MemoryHighSignalState= system_high_memory_signal_state,
+		@MemoryLowSignalState  = system_low_memory_signal_state
+	FROM    sys.dm_os_sys_memory
+END
+IF EXISTS(SELECT * FROM @counters WHERE object_name='sys.dm_os_nodes')
+BEGIN
+	SELECT @CountOfNodesWithThreadResourcesLow=COUNT(*)   
+	FROM sys.dm_os_nodes
+	WHERE node_state_desc LIKE '%THREAD_RESOURCES_LOW%'
+END
+
+SELECT SYSUTCDATETIME() AS SnapshotDate,
+		'sys.dm_os_nodes' AS object_name,
+		'Count of Nodes reporting thread resources low' AS counter_name,
+		'' AS instance_name,
+		@CountOfNodesWithThreadResourcesLow AS  cntr_value,
+		65792 AS cntr_type
+WHERE @CountOfNodesWithThreadResourcesLow IS NOT NULL
+UNION ALL
+SELECT SYSUTCDATETIME() AS SnapshotDate,
+		'sys.dm_os_sys_memory' AS object_name,
+		'Available Physical Memory (KB)' AS counter_name,
+		'' AS instance_name,
+		@AvailablePhysicalMemory AS  cntr_value,
+		65792 AS cntr_type
+WHERE @AvailablePhysicalMemory IS NOT NULL
+UNION ALL
+SELECT SYSUTCDATETIME()AS SnapshotDate,
+		'sys.dm_os_sys_memory' AS object_name,
+		'System Low Memory Signal State' AS counter_name,
+		'' AS instance_name,
+		@MemoryLowSignalState AS  cntr_value,
+		65792 AS cntr_type
+WHERE @MemoryLowSignalState IS NOT NULL
+UNION ALL
+SELECT SYSUTCDATETIME() AS SnapshotDate,
+		'sys.dm_os_sys_memory' AS object_name,
+		'System High Memory Signal State' AS counter_name,
+		'' AS instance_name,
+		@MemoryHighSignalState AS  cntr_value,
+		65792 AS cntr_type
+WHERE @MemoryHighSignalState IS NOT NULL
+UNION ALL
 SELECT SYSUTCDATETIME() AS SnapshotDate,
 		STUFF(pc.object_name,1,CHARINDEX(':',pc.object_name),'') AS object_name,
        pc.counter_name,
