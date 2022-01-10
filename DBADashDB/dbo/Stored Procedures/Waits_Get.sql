9f6ff0e5393ef8edd3d2db3dcab1393744174805
David Wiseman <dave_wiseman@trimble.com>
2022-01-10 15:56:50 +0000

    Add additional metrics to wait chart

--- DBADashDB/dbo/Stored Procedures/Waits_Get.sql
+++ DBADashDB/dbo/Stored Procedures/Waits_Get.sql
@@ -17,6 +17,10 @@ IF @ToDate IS NULL
 DECLARE @SQL NVARCHAR(MAX)
 DECLARE @DateGroupingSQL NVARCHAR(MAX)
 DECLARE @DateGroupingJoin NVARCHAR(MAX)
+DECLARE @SchedulerCount INT 
+
+SELECT @SchedulerCount = I.scheduler_count
+FROM dbo.Instances I WHERE I.InstanceID = @InstanceID
 
 SELECT @DateGroupingSQL= CASE WHEN @DateGroupingMin IS NULL OR @DateGroupingMin=0 THEN 'W.SnapshotDate'
 			ELSE 'DG.DateGroup' END,
@@ -48,34 +52,80 @@ END
 CREATE TABLE #WaitGrp(
 	[Time] DATETIME2(2) NOT NULL,
 	WaitTypeID SMALLINT NOT NULL,
-	WaitTimeMsPerSec DECIMAL(19,5) NULL
+	TotalWaitMs BIGINT NOT NULL,
+	SignalWaitMs BIGINT NOT NULL,
+	WaitingTasksCount BIGINT NOT NULL
 )
+CREATE TABLE #Time(
+	[Time] DATETIME2(2) NOT NULL,
+	SampleDurationMs BIGINT NULL
+)
+/* 
+	Wait types with zero wait are not recorded so we use the max of the sample_ms_diff for each wait type/time, then sum for the date grouping.
+*/
+SET @SQL = '
+WITH W AS (
+	SELECT W.SnapshotDate,
+			MAX(W.sample_ms_diff) AS sample_ms_diff
+	FROM dbo.Waits' + CASE WHEN @Use60MIN =1 THEN '_60MIN' ELSE '' END + ' W 	
+	WHERE W.SnapshotDate>= @FromDate
+	AND W.SnapshotDate < @ToDate
+	AND W.InstanceID=@InstanceID
+	GROUP BY W.SnapshotDate
+)
+SELECT ' + @DateGroupingSQL + ' AS [Time],
+			SUM(W.sample_ms_diff) AS SampleDurationMs
+FROM W
+' + @DateGroupingJoin + '
+GROUP BY ' + @DateGroupingSQL 
+
+INSERT INTO #Time
+(
+    Time,
+    SampleDurationMs
+)
+EXEC sp_executesql @SQL,N'@FromDate DATETIME2(2),@ToDate DATETIME2(2),@InstanceID INT,@Top INT,@DateGroupingMin INT',@FromDate,@ToDate,@InstanceID,@Top,@DateGroupingMin;
+
 
 SET @SQL = N'
 SELECT ' + @DateGroupingSQL + ' AS [Time],
 			W.WaitTypeID,
-			SUM(W.wait_time_ms)*1000.0 / SUM(W.sample_ms_diff) AS WaitTimeMsPerSec
+			SUM(W.wait_time_ms) AS TotalWaitMs,
+			SUM(W.signal_wait_time_ms) AS SignalWaitMs,
+			SUM(W.waiting_tasks_count) AS WaitingTasksCount
 FROM dbo.Waits' + CASE WHEN @Use60MIN =1 THEN '_60MIN' ELSE '' END + ' W 
 ' + @DateGroupingJoin + '
 JOIN dbo.WaitType WT ON WT.WaitTypeID = W.WaitTypeID
 WHERE W.SnapshotDate>= @FromDate
-AND W.SnapshotDate <= @ToDate
+AND W.SnapshotDate < @ToDate
 AND W.InstanceID=@InstanceID
 ' + CASE WHEN @CriticalWaitsOnly=1 THEN 'AND WT.IsCriticalWait=1' ELSE '' END + '
 ' + CASE WHEN @WaitType IS NULL THEN '' ELSE 'AND WT.WaitType LIKE @WaitType' END + '
+' + CASE WHEN @WaitTypeID IS NULL THEN '' ELSE 'AND WT.WaitTypeID = @WaitTypeID' END + '
 AND WT.IsExcluded = 0
 GROUP BY W.WaitTypeID, ' + @DateGroupingSQL 
 
-INSERT INTO #WaitGrp([Time],WaitTypeID,WaitTimeMsPerSec)
-EXEC sp_executesql @SQL,N'@FromDate DATETIME2(2),@ToDate DATETIME2(2),@InstanceID INT,@Top INT,@DateGroupingMin INT,@WaitType NVARCHAR(60)',@FromDate,@ToDate,@InstanceID,@Top,@DateGroupingMin,@WaitType;
+INSERT INTO #WaitGrp([Time],WaitTypeID,TotalWaitMs,SignalWaitMs,WaitingTasksCount)
+EXEC sp_executesql @SQL,N'@FromDate DATETIME2(2),@ToDate DATETIME2(2),@InstanceID INT,@Top INT,@DateGroupingMin INT,@WaitType NVARCHAR(60),@WaitTypeID INT',@FromDate,@ToDate,@InstanceID,@Top,@DateGroupingMin,@WaitType,@WaitTypeID;
 
 IF @WaitTypeID IS NOT NULL -- Filtering for a specific wait type
 BEGIN
 	/*	If a wait doesn't accumulate for a time period it won't be present.  
 		This query will fill in the blanks with zeros when we are filtering for a specific wait type - otherwise the charts can look misleading.
 	*/
-	SELECT T.Time,WT.WaitType,ISNULL(W.WaitTimeMsPerSec,0) AS WaitTimeMsPerSec
-	FROM (SELECT DISTINCT [Time] FROM #WaitGrp) T 
+	SELECT T.Time,
+			WT.WaitType,
+			ISNULL(W.TotalWaitMs/1000.0,0) AS TotalWaitSec,
+			ISNULL(W.SignalWaitMs/1000.0,0) AS SignalWaitSec,
+			ISNULL(W.SignalWaitMs*100.0/NULLIF(W.TotalWaitMs,0),0) AS SignalWaitPct,
+			ISNULL(W.TotalWaitMs / (T.SampleDurationMs/1000.0),0) AS WaitTimeMsPerSec,
+			ISNULL(W.SignalWaitMs / (T.SampleDurationMs/1000.0),0) AS SignalWaitMsPerSec,
+			ISNULL(W.TotalWaitMs / (T.SampleDurationMs/1000.0) / @SchedulerCount,0) AS WaitTimeMsPerCorePerSec,
+			ISNULL(W.SignalWaitMs / (T.SampleDurationMs/1000.0) / @SchedulerCount,0) AS SignalWaitMsPerCorePerSec,
+			T.SampleDurationMs /1000 SampleDurationSec,
+			W.TotalWaitMs*1.0 /ISNULL(NULLIF(W.WaitingTasksCount,0),1)  AS AvgWaitTimeMs,
+			ISNULL(W.WaitingTasksCount,0) AS WaitingTasksCount
+	FROM #Time T
 	CROSS JOIN dbo.WaitType WT 
 	LEFT JOIN #WaitGrp W ON W.Time = T.Time AND WT.WaitTypeID = W.WaitTypeID
 	WHERE WT.WaitTypeID = @WaitTypeID
@@ -86,17 +136,28 @@ BEGIN
 	/* 
 		Show the top waits for each time period and group other waits as "{Other}"
 	*/
-	WITH T AS (
-		SELECT *,ROW_NUMBER() OVER(PARTITION BY [Time] ORDER BY WaitTimeMsPerSec DESC) rnum
+	WITH W AS (
+		SELECT *,ROW_NUMBER() OVER(PARTITION BY [Time] ORDER BY T1.TotalWaitMs DESC) rnum
 		FROM #WaitGrp T1
 	)
-	SELECT [Time],
+	SELECT T.[Time],
 		CASE WHEN rnum> @Top THEN '{Other}' WHEN WT.IsCriticalWait=1 THEN '!!'  + WT.WaitType ELSE WT.WaitType END as WaitType,
-		SUM(WaitTimeMsPerSec) as WaitTimeMsPerSec
-	FROM T 
-	JOIN dbo.WaitType WT ON WT.WaitTypeID = T.WaitTypeID
-	GROUP BY [Time],CASE WHEN rnum> @Top THEN '{Other}' WHEN WT.IsCriticalWait=1 THEN '!!'  + WT.WaitType ELSE WT.WaitType END
+		SUM(W.TotalWaitMs)/1000.0 AS TotalWaitSec,
+        SUM(W.SignalWaitMs)/1000.0 AS SignalWaitSec,
+		SUM(W.SignalWaitMs)*100.0 / NULLIF(SUM(W.TotalWaitMs),0) AS SignalWaitPct,
+		SUM(W.TotalWaitMs) / SUM(T.SampleDurationMs/1000.0) AS WaitTimeMsPerSec,
+		SUM(W.SignalWaitMs) / SUM(T.SampleDurationMs/1000.0) AS SignalWaitMsPerSec,
+		SUM(W.TotalWaitMs) / SUM(T.SampleDurationMs/1000.0) / @SchedulerCount AS WaitTimeMsPerCorePerSec,
+		SUM(W.SignalWaitMs) / SUM(T.SampleDurationMs/1000.0) / @SchedulerCount AS SignalWaitMsPerCorePerSec,
+		SUM(T.SampleDurationMs) /1000 SampleDurationSec,
+        SUM(W.TotalWaitMs*1.0) /ISNULL(NULLIF(SUM(WaitingTasksCount),0),1)  AS AvgWaitTimeMs,
+		SUM(W.WaitingTasksCount) AS WaitingTasksCount
+	FROM W 
+	JOIN #Time T ON T.Time = W.Time
+	JOIN dbo.WaitType WT ON WT.WaitTypeID = W.WaitTypeID
+	GROUP BY T.[Time],CASE WHEN rnum> @Top THEN '{Other}' WHEN WT.IsCriticalWait=1 THEN '!!'  + WT.WaitType ELSE WT.WaitType END
 	ORDER BY WaitType,T.Time
 END
 
 DROP TABLE #WaitGrp
+GO
