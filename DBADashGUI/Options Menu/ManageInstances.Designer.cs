c955f7ed1075c432509d71fc3b1234719a674d81
David Wiseman <dave_wiseman@trimble.com>
2022-03-02 09:47:49 +0000

    Add filter to manage instances dialog
    Adding text filter and active/deleted filter.
    #65

--- DBADashGUI/Options Menu/ManageInstances.Designer.cs
+++ DBADashGUI/Options Menu/ManageInstances.Designer.cs
@@ -28,14 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ManageInstances));
             this.dgv = new System.Windows.Forms.DataGridView();
             this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.txtSearch = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.showActiveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showDeletedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tsFilterError = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.colInstance = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.colIsAzure = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colIsAzure = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             this.colDeleteRestore = new System.Windows.Forms.DataGridViewLinkColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dgv
@@ -49,14 +61,14 @@
             this.colIsAzure,
             this.colDeleteRestore});
             this.dgv.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dgv.Location = new System.Drawing.Point(0, 0);
+            this.dgv.Location = new System.Drawing.Point(0, 27);
             this.dgv.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dgv.Name = "dgv";
             this.dgv.ReadOnly = true;
             this.dgv.RowHeadersVisible = false;
             this.dgv.RowHeadersWidth = 51;
             this.dgv.RowTemplate.Height = 24;
-            this.dgv.Size = new System.Drawing.Size(621, 860);
+            this.dgv.Size = new System.Drawing.Size(621, 811);
             this.dgv.TabIndex = 0;
             this.dgv.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_CellContentClick);
             this.dgv.RowsAdded += new System.Windows.Forms.DataGridViewRowsAddedEventHandler(this.dgv_RowsAdded);
@@ -79,6 +91,88 @@
             this.dataGridViewTextBoxColumn2.ReadOnly = true;
             this.dataGridViewTextBoxColumn2.Width = 96;
             // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.txtSearch,
+            this.toolStripLabel1,
+            this.toolStripDropDownButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(621, 27);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(150, 27);
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(56, 24);
+            this.toolStripLabel1.Text = "Search:";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showActiveToolStripMenuItem,
+            this.showDeletedToolStripMenuItem});
+            this.toolStripDropDownButton1.Image = global::DBADashGUI.Properties.Resources.FilterDropdown_16x;
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(34, 24);
+            this.toolStripDropDownButton1.Text = "toolStripDropDownButton1";
+            // 
+            // showActiveToolStripMenuItem
+            // 
+            this.showActiveToolStripMenuItem.Checked = true;
+            this.showActiveToolStripMenuItem.CheckOnClick = true;
+            this.showActiveToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.showActiveToolStripMenuItem.Name = "showActiveToolStripMenuItem";
+            this.showActiveToolStripMenuItem.Size = new System.Drawing.Size(185, 26);
+            this.showActiveToolStripMenuItem.Text = "Show Active";
+            this.showActiveToolStripMenuItem.Click += new System.EventHandler(this.showActiveToolStripMenuItem_Click);
+            // 
+            // showDeletedToolStripMenuItem
+            // 
+            this.showDeletedToolStripMenuItem.Checked = true;
+            this.showDeletedToolStripMenuItem.CheckOnClick = true;
+            this.showDeletedToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.showDeletedToolStripMenuItem.Name = "showDeletedToolStripMenuItem";
+            this.showDeletedToolStripMenuItem.Size = new System.Drawing.Size(185, 26);
+            this.showDeletedToolStripMenuItem.Text = "Show Deleted";
+            this.showDeletedToolStripMenuItem.Click += new System.EventHandler(this.showDeletedToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsFilterError});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 838);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(621, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tsFilterError
+            // 
+            this.tsFilterError.Name = "tsFilterError";
+            this.tsFilterError.Size = new System.Drawing.Size(51, 20);
+            this.tsFilterError.Text = "{Error}";
+            this.tsFilterError.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // colInstance
             // 
             this.colInstance.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
@@ -95,6 +189,8 @@
             this.colIsAzure.MinimumWidth = 6;
             this.colIsAzure.Name = "colIsAzure";
             this.colIsAzure.ReadOnly = true;
+            this.colIsAzure.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.colIsAzure.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
             this.colIsAzure.Width = 80;
             // 
             // colDeleteRestore
@@ -111,6 +207,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(621, 860);
             this.Controls.Add(this.dgv);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "ManageInstances";
@@ -118,7 +216,12 @@
             this.Text = "Manage Instances";
             this.Load += new System.EventHandler(this.ManageInstances_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -127,8 +230,17 @@
         private System.Windows.Forms.DataGridView dgv;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripTextBox txtSearch;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tsFilterError;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem showActiveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showDeletedToolStripMenuItem;
         private System.Windows.Forms.DataGridViewTextBoxColumn colInstance;
-        private System.Windows.Forms.DataGridViewTextBoxColumn colIsAzure;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn colIsAzure;
         private System.Windows.Forms.DataGridViewLinkColumn colDeleteRestore;
     }
 }
--- DBADashGUI/Options Menu/ManageInstances.cs
+++ DBADashGUI/Options Menu/ManageInstances.cs
@@ -16,6 +16,7 @@ namespace DBADashGUI
         public ManageInstances()
         {
             InitializeComponent();
+            tsFilterError.ForeColor = DashColors.Fail;
         }
 
         public string Tags { get; set; } = null;
@@ -36,7 +37,7 @@ namespace DBADashGUI
         {
             var dt = CommonData.GetInstances(Tags, null);
             dgv.AutoGenerateColumns = false;
-            dgv.DataSource = dt;
+            dgv.DataSource = new DataView(dt);
 
         }
 
@@ -89,6 +90,61 @@ namespace DBADashGUI
                 activeFlagChanged = true;
                 
             }
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer1.Start();
+        }
+
+        private void setFilter()
+        {
+            try
+            {
+                StringBuilder sbFilter = new StringBuilder();
+                if (txtSearch.Text.Trim().Length > 0)
+                {
+                    sbFilter.AppendFormat("AND ConnectionID LIKE '*{0}*'", txtSearch.Text.Replace("'", "''"));
+                }
+                if (showActiveToolStripMenuItem.Checked != showDeletedToolStripMenuItem.Checked)
+                {
+                    sbFilter.AppendFormat("AND IsActive = {0}", showActiveToolStripMenuItem.Checked ? 1 : 0);
+                }
+                else if (!showActiveToolStripMenuItem.Checked && !showDeletedToolStripMenuItem.Checked)
+                {
+                    sbFilter.AppendFormat("AND 1=0"); // Not showing active or deleted items.  Return no rows
+                }
+
+                if (sbFilter.Length > 0) { 
+                    sbFilter.Remove(0, 4); // Remove AND 
+                }
+
+                ((DataView)dgv.DataSource).RowFilter = sbFilter.ToString();
+                tsFilterError.Visible = false;
+            }
+            catch (Exception ex)
+            {
+                tsFilterError.Text = "Filter error:" + ex.Message;
+                tsFilterError.Visible = true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            setFilter();
+        }
+
+    
+        private void showActiveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            setFilter();
+        }
+
+        private void showDeletedToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            setFilter();
         }
     }
 }
