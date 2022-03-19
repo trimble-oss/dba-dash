611798afee863be1c3746b6d18eca8e1899f2aba
David Wiseman <dave_wiseman@trimble.com>
2022-03-19 20:14:04 +0000

    Add Grid + Export options to Drive History
    Add grid view.  Add export to Excel & Copy Data. #85

--- DBADashDB/dbo/Stored Procedures/DriveSnapshot_Get.sql
+++ DBADashDB/dbo/Stored Procedures/DriveSnapshot_Get.sql
@@ -8,6 +8,8 @@ AS
 SELECT	DG.DateGroup AS SnapshotDate,
 		MAX(DSS.Capacity)/POWER(1024.0,3) AS SizeGB,
 		MAX(DSS.UsedSpace)/POWER(1024.0,3) AS UsedGB,
+		MIN(DSS.FreeSpace)/POWER(1024.0,3) AS FreeGB,
+		MIN(DSS.FreeSpace*1.0)/MAX(DSS.Capacity) AS FreePct,
 		COUNT(*) AS Samples
 FROM dbo.DriveSnapshot DSS
 CROSS APPLY dbo.DateGroupingMins(DSS.SnapshotDate,@DateGroupingMins) DG
