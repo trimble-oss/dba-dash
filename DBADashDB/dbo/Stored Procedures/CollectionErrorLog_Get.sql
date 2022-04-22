0bdae6ae5f8eaad01cae183b482f1fad426a3710
David Wiseman <dave_wiseman@trimble.com>
2022-04-22 11:24:15 +0100

    Improve DBA Dash Error Log
    - Add filtering
    - Convert Message column to a link where you can click to view full error text.

--- DBADashDB/dbo/Stored Procedures/CollectionErrorLog_Get.sql
+++ DBADashDB/dbo/Stored Procedures/CollectionErrorLog_Get.sql
@@ -1,7 +1,11 @@
 ﻿CREATE PROC dbo.CollectionErrorLog_Get(
 	@InstanceID INT=NULL,
 	@Days INT=7,
-	@InstanceGroupName NVARCHAR(128)=NULL
+	@InstanceGroupName NVARCHAR(128)=NULL,
+	@ErrorSource VARCHAR(100)=NULL,
+	@ErrorContext VARCHAR(100)=NULL,
+	@ErrorMessage VARCHAR(200)=NULL,
+	@InstanceDisplayName NVARCHAR(128)=NULL
 )
 AS
 SELECT I.Instance,
@@ -15,5 +19,10 @@ FROM dbo.CollectionErrorLog E
 LEFT JOIN dbo.Instances I ON I.InstanceID = E.InstanceID
 WHERE (I.InstanceID = @InstanceID OR @InstanceID IS NULL)
 AND (I.InstanceGroupName = @InstanceGroupName OR @InstanceGroupName IS NULL)
+AND (I.InstanceDisplayName = @InstanceDisplayName OR @InstanceDisplayName IS NULL)
 AND E.ErrorDate>=DATEADD(d,-@Days,GETUTCDATE())
+AND (E.ErrorSource = @ErrorSource OR @ErrorSource IS NULL)
+AND (E.ErrorMessage LIKE '%' + @ErrorMessage + '%' OR @ErrorMessage IS NULL)
+AND (E.ErrorContext = @ErrorContext OR @ErrorContext IS NULL)
 ORDER BY E.ErrorDate DESC
+OPTION(RECOMPILE)
--- DBADashGUI/CollectionDates/CollectionErrors.cs
+++ DBADashGUI/CollectionDates/CollectionErrors.cs
@@ -16,6 +16,14 @@ namespace DBADashGUI.CollectionDates
         public CollectionErrors()
         {
             InitializeComponent();
+            Common.StyleGrid(ref dgvDBADashErrors);
+            txtContext.BackColor = DashColors.BluePale;
+            txtMessage.BackColor = DashColors.BluePale;
+            txtSource.BackColor = DashColors.BluePale;
+            setFilterHighlight(txtSource, sourceToolStripMenuItem);
+            setFilterHighlight(txtContext, contextToolStripMenuItem);
+            setFilterHighlight(txtMessage, messageToolStripMenuItem);
+            setFilterHighlight(txtInstance, instanceToolStripMenuItem);
         }
 
         public Int32 InstanceID { get; set; }
@@ -49,35 +57,87 @@ namespace DBADashGUI.CollectionDates
             }
         }
 
-        public void RefreshData()
+        private static DataTable getErrorLog(int instanceID,string instanceGroupName,string instanceDisplayName,string errorSource,string errorContext,string errorMessage,int Days)
         {
             using (var cn = new SqlConnection(Common.ConnectionString))
             using (var cmd = new SqlCommand("dbo.CollectionErrorLog_Get", cn) { CommandType = CommandType.StoredProcedure })
             using (var da = new SqlDataAdapter(cmd))
             {
-                    if (InstanceID > 0)
-                    {
-                        cmd.Parameters.AddWithValue("InstanceID", InstanceID);
-                    }
-                    if (!String.IsNullOrEmpty(InstanceGroupName))
-                    {
-                        cmd.Parameters.AddWithValue("InstanceGroupName", InstanceGroupName);
-                    }
-                    cmd.Parameters.AddWithValue("Days", Days);
-                    DataTable dt = new DataTable();
-                    da.Fill(dt);
-                    Common.ConvertUTCToLocal(ref dt);
-                    dgvDBADashErrors.AutoGenerateColumns = false;
-                    dgvDBADashErrors.DataSource = dt;
-                    dgvDBADashErrors.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.DisplayedCells);
-                
+                if (instanceID > 0)
+                {
+                    cmd.Parameters.AddWithValue("InstanceID", instanceID);
+                }
+                if (!String.IsNullOrEmpty(instanceGroupName))
+                {
+                    cmd.Parameters.AddWithValue("InstanceGroupName", instanceGroupName);
+                }
+                if (!String.IsNullOrEmpty(instanceDisplayName))
+                {
+                    cmd.Parameters.AddWithValue("InstanceDisplayName", instanceDisplayName);
+                }
+                if (!String.IsNullOrEmpty(errorSource))
+                {
+                    cmd.Parameters.AddWithValue("ErrorSource", errorSource);
+                }
+                if (!String.IsNullOrEmpty(errorContext))
+                {
+                    cmd.Parameters.AddWithValue("ErrorContext", errorContext);
+                }
+                if (!String.IsNullOrEmpty(errorMessage))
+                {
+                    cmd.Parameters.AddWithValue("ErrorMessage", errorMessage);
+                }
+                cmd.Parameters.AddWithValue("Days", Days);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                Common.ConvertUTCToLocal(ref dt);
+                return dt;
+            }
+        }
+
+        public void RefreshData()
+        {
+            clearFilters();
+            refreshData();           
+        }
+
+        private void refreshData()
+        {
+            tsFilter.Font = isFiltered ? new Font(tsFilter.Font, FontStyle.Bold) : new Font(tsFilter.Font, FontStyle.Regular);
+            DataTable dt = getErrorLog(InstanceID, InstanceGroupName, txtInstance.Text.Trim(), txtSource.Text.Trim(), txtContext.Text.Trim(), txtMessage.Text.Trim(), Days);
+            dgvDBADashErrors.AutoGenerateColumns = false;
+            dgvDBADashErrors.DataSource = dt;
+            dgvDBADashErrors.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.DisplayedCells);
+        }
+
+        private bool isFiltered
+        {
+            get
+            {
+                if (!String.IsNullOrEmpty(txtSource.Text.Trim()))
+                {
+                    return true;
+                }
+                if (!String.IsNullOrEmpty(txtContext.Text.Trim()))
+                {
+                    return true;
+                }
+                if (!String.IsNullOrEmpty(txtMessage.Text.Trim()))
+                {
+                    return true;
+                }
+                if (!String.IsNullOrEmpty(txtInstance.Text.Trim()))
+                {
+                    return true;
+                }
+                return false;
             }
         }
 
         private void tsErrorDays_Click(object sender, EventArgs e)
         {
             Days = int.Parse((string)((ToolStripMenuItem)sender).Tag);
-            RefreshData();
+            refreshData();
         }
 
         private void tsCopyErrors_Click(object sender, EventArgs e)
@@ -87,7 +147,7 @@ namespace DBADashGUI.CollectionDates
 
         private void tsRefreshErrors_Click(object sender, EventArgs e)
         {
-            RefreshData();
+            refreshData();
         }
 
         private void tsAckErrors_Click(object sender, EventArgs e)
@@ -98,19 +158,97 @@ namespace DBADashGUI.CollectionDates
         private void AcknowledgeErrors()
         {
             using(var cn = new SqlConnection(Common.ConnectionString))
+            using(var cmd = new SqlCommand("dbo.AcknowledgeErrors", cn) { CommandType = CommandType.StoredProcedure })
             {
-                cn.Open();
-                using (var cmd = new SqlCommand("dbo.AcknowledgeErrors", cn) { CommandType = CommandType.StoredProcedure })
-                {
-                    cmd.ExecuteNonQuery();
-                    MessageBox.Show("Errors up to current date acknowledged and cleared from summary page.", "Errors acknowledged", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                }
+                cn.Open();                          
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Errors up to current date acknowledged and cleared from summary page.", "Errors acknowledged", MessageBoxButtons.OK, MessageBoxIcon.Information);               
             }
         }
 
         private void tsExcel_Click(object sender, EventArgs e)
         {
             Common.PromptSaveDataGridView(ref dgvDBADashErrors);
+        }
+
+        private void dgvDBADashErrors_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                if (e.ColumnIndex == ErrorMessage.Index)
+                {
+                    string message = Convert.ToString(dgvDBADashErrors.Rows[e.RowIndex].Cells[e.ColumnIndex].Value);
+                    NotepadHelper.ShowMessage(message, "DBA Dash Error");
+                }
+                else if (e.ColumnIndex == ErrorSource.Index)
+                {
+                    txtSource.Text = Convert.ToString(dgvDBADashErrors.Rows[e.RowIndex].Cells[e.ColumnIndex].Value);
+                    refreshData();
+                }
+                else if (e.ColumnIndex== ErrorContext.Index)
+                {
+                    txtContext.Text = Convert.ToString(dgvDBADashErrors.Rows[e.RowIndex].Cells[e.ColumnIndex].Value);
+                    refreshData();
+                }
+                else if (e.ColumnIndex== Instance.Index)
+                {
+                    txtInstance.Text= Convert.ToString(dgvDBADashErrors.Rows[e.RowIndex].Cells[e.ColumnIndex].Value);
+                    refreshData();
+                }
+            }
+        }
+
+        private void clearFilters()
+        {
+            txtSource.Text = String.Empty;
+            txtMessage.Text = String.Empty;
+            txtContext.Text = String.Empty;
+            txtInstance.Text=String.Empty;
+        }
+
+        private void tsClearFilters_Click(object sender, EventArgs e)
+        {
+            clearFilters();
+            refreshData();
+        }
+
+        private void txtSource_TextChanged(object sender, EventArgs e)
+        {
+            setFilterHighlight(txtSource, sourceToolStripMenuItem);
+        }
+
+        private void txtContext_TextChanged(object sender, EventArgs e)
+        {
+            setFilterHighlight(txtContext, contextToolStripMenuItem);
+        }
+
+        private void txtMessage_TextChanged(object sender, EventArgs e)
+        {
+            setFilterHighlight(txtMessage, messageToolStripMenuItem);
+        }
+        private void txtInstance_TextChanged(object sender, EventArgs e)
+        {
+            setFilterHighlight(txtInstance, instanceToolStripMenuItem);
+        }
+
+        private void Filter_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                refreshData();
+            }
+        }
+
+        private void setFilterHighlight(ToolStripTextBox txt, ToolStripMenuItem lbl)
+        {
+            if (!String.IsNullOrEmpty(txt.Text.Trim()))
+            {
+                lbl.Font = new Font(lbl.Font, FontStyle.Bold);
+            }
+            else
+            {
+                lbl.Font = new Font(lbl.Font, FontStyle.Regular);
+            }
         }
     }
 }
