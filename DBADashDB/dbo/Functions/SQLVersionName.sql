2cbe38a263808e3882c91bbac01250f8b1163e73
David Wiseman <wiseman1982@googlemail.com>
2022-05-27 21:03:24 +0100

    SQL 2022 Support
    Add version 16 (2022) to supported list
    Update SQLVersionName function for SQL 2022
    Update system config options with new options
    Update DB config options with new options
    #206

--- DBADashDB/dbo/Functions/SQLVersionName.sql
+++ DBADashDB/dbo/Functions/SQLVersionName.sql
@@ -11,4 +11,5 @@ SELECT 	CASE WHEN @EditionID = 1674378470 AND @ProductVersion LIKE '%.%' THEN 'A
 			WHEN @ProductVersion LIKE '13.%' THEN 'SQL 2016'
 			WHEN @ProductVersion LIKE '14.%' THEN 'SQL 2017'
 			WHEN @ProductVersion LIKE '15.%' THEN 'SQL 2019'
+			WHEN @ProductVersion LIKE '16.%' THEN 'SQL 2022'
 			ELSE @ProductVersion END AS SQLVersionName
