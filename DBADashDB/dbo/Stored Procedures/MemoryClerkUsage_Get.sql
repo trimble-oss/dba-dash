862364c73d549fba78819bdfd5482ec0b8b41b28
David Wiseman <dave_wiseman@trimble.com>
2022-06-08 13:54:15 +0100

    Memory tab improvement
    View memory clerk usage over longer than 2 days.
    Add Plan Cache object to memory counters
    #218

--- DBADashDB/dbo/Stored Procedures/MemoryClerkUsage_Get.sql
+++ DBADashDB/dbo/Stored Procedures/MemoryClerkUsage_Get.sql
@@ -2,21 +2,52 @@
 	@InstanceID INT,
 	@FromDate DATETIME=NULL,
 	@ToDate DATETIME=NULL,
-	@MemoryClerkType NVARCHAR(60)
+	@MemoryClerkType NVARCHAR(60),
+	@Mins INT=NULL,
+	@Agg VARCHAR(20)='NONE', -- Options: AVG,MIN,MAX,NONE
+	@Measure NVARCHAR(128)='pages_kb', -- Options: pages_kb,virtual_memory_reserved_kb,virtual_memory_committed_kb,awe_allocated_kb,shared_memory_reserved_kb,shared_memory_committed_kb
+	@Debug BIT=0
 )
 AS
-SELECT MCT.MemoryClerkType,
-       MU.pages_kb,
-       MU.virtual_memory_reserved_kb,
-       MU.virtual_memory_committed_kb,
-       MU.awe_allocated_kb,
-       MU.shared_memory_reserved_kb,
-       MU.shared_memory_committed_kb,
-	   MU.SnapshotDate
+DECLARE @SQLAgg NVARCHAR(MAX)
+DECLARE @MeasureValidated NVARCHAR(MAX) = CASE WHEN @Measure IN('pages_kb','virtual_memory_reserved_kb','virtual_memory_committed_kb','awe_allocated_kb','shared_memory_reserved_kb','shared_memory_committed_kb') THEN @Measure ELSE NULL END
+DECLARE @AggValidated NVARCHAR(MAX) = CASE WHEN @Mins IS NULL OR @Mins<=0 THEN 'NONE' WHEN @Agg IN('AVG','MIN','MAX','NONE') THEN @Agg ELSE NULL END
+IF @AggValidated ='NONE' AND @Mins>0
+BEGIN;
+	THROW 50001,'Invalid @Agg/@Mins combination',1;
+	RETURN
+END
+IF @AggValidated IS NULL
+BEGIN;
+	THROW 50002,'Invalid @Agg',1;
+	RETURN
+END
+IF @MeasureValidated IS NULL
+BEGIN;
+	THROW 50003,'Invalid @Measure',1;
+	RETURN
+END
+DECLARE @MeasureSQL NVARCHAR(MAX) = CASE WHEN @AggValidated = 'NONE' THEN 'MU.' + QUOTENAME(@MeasureValidated)
+									ELSE @AggValidated + '(MU.' + QUOTENAME(@MeasureValidated) + ') AS ' + QUOTENAME(@MeasureValidated) END
+
+DECLARE @SQL NVARCHAR(MAX)
+SET @SQL = N'
+SELECT MCT.MemoryClerkType, 
+	   ' + CASE WHEN @AggValidated = 'NONE' THEN 'MU.SnapshotDate,' ELSE 'DG.DateGroup AS SnapshotDate,' END + '
+	   ' + @MeasureSQL + '
 FROM dbo.MemoryUsage MU
 JOIN dbo.MemoryClerkType MCT ON MCT.MemoryClerkTypeID = MU.MemoryClerkTypeID
+'+ CASE WHEN @AggValidated='NONE' THEN '' ELSE 'CROSS APPLY dbo.DateGroupingMins(MU.SnapshotDate,@Mins) DG' END + '
 WHERE MCT.MemoryClerkType = @MemoryClerkType
 AND MU.SnapshotDate >= @FromDate
 AND MU.SnapshotDate < @ToDate
 AND MU.InstanceID = @InstanceID
-ORDER BY MU.SnapshotDate
+' + CASE WHEN @AggValidated = 'NONE' THEN '' ELSE 'GROUP BY MCT.MemoryClerkType,DG.DateGroup' END + '
+ORDER BY SnapshotDate'
+
+IF @Debug=1
+BEGIN
+	PRINT @SQL
+END
+
+EXEC sp_executesql @SQL,N'@InstanceID INT,@FromDate DATETIME2(2),@ToDate DATETIME2(2),@MemoryClerkType NVARCHAR(60),@Mins INT',@InstanceID,@FromDate,@ToDate,@MemoryClerkType,@Mins
--- DBADashDB/dbo/Stored Procedures/MemoryCounters_Get.sql
+++ DBADashDB/dbo/Stored Procedures/MemoryCounters_Get.sql
@@ -8,4 +8,5 @@ WHERE   (   object_name='Buffer Manager'
             OR object_name = 'Memory Manager'
             OR counter_name='Count of Nodes reporting thread resources low'
             OR object_name = 'sys.dm_os_sys_memory'
+            OR object_name = 'Plan Cache'
         )
