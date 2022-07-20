c43d2b0b33708fd6e7d8b3d7743ab394328d0daa
David Wiseman <33157668+DavidWiseman@users.noreply.github.com>
2022-07-20 11:53:08 +0100

    Bump version

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -22,4 +22,4 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.19.1")]
+[assembly: AssemblyVersion("2.20.0")]
