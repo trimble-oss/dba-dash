1128f67df4bed35f8c31403d45f93f1a08c0533b
David Wiseman <dave_wiseman@trimble.com>
2022-07-27 14:26:32 +0100

    AvailabilityReplicas - Allow NULL value for endpoint_url
    Fix Cannot insert the value NULL into column 'endpoint_url' error.  #274

--- DBADashDB/dbo/User Defined Types/AvailabilityReplicas.sql
+++ DBADashDB/dbo/User Defined Types/AvailabilityReplicas.sql
@@ -3,7 +3,7 @@
        group_id UNIQUEIDENTIFIER NOT NULL,
        replica_metadata_id INT NULL,
        replica_server_name NVARCHAR(256) NOT NULL,
-       endpoint_url NVARCHAR(128) NOT NULL,
+       endpoint_url NVARCHAR(128) NULL,
        availability_mode TINYINT NOT NULL,
        failover_mode TINYINT NOT NULL,
        session_timeout INT NOT NULL,
