e81024ce271f937b01c71360162469190435923d
David Wiseman <dave_wiseman@trimble.com>
2022-09-17 08:44:48 +0100

    Schedule editing improvement
    Added bulk editing of collection schedules.
    #305

--- DBADashServiceConfig/ScheduleConfig.cs
+++ DBADashServiceConfig/ScheduleConfig.cs
@@ -11,6 +11,7 @@ using System.Windows.Forms;
 using DBADash;
 using DBADashService;
 using Humanizer;
+using Microsoft.SqlServer.Management.Smo;
 using Newtonsoft.Json;
 using Quartz;
 
@@ -25,6 +26,12 @@ namespace DBADashServiceConfig
 
         CollectionSchedules userSchedule;
         public CollectionSchedules BaseSchedule = CollectionSchedules.DefaultSchedules;
+
+        // For copy/paste function 
+        private string copySchedule;
+        private bool copyDefault;
+        private bool copyRunOnStart;
+        private bool hasCopy;
 
         public CollectionSchedules ConfiguredSchedule
         {
@@ -48,32 +55,32 @@ namespace DBADashServiceConfig
         private void ScheduleConfig_Load(object sender, EventArgs e)
         {
             dgv.Columns.Clear();
-            dgv.Columns.Add(new DataGridViewTextBoxColumn() { Name = "CollectionType", HeaderText = "Collection Type", ReadOnly = true });
-            dgv.Columns.Add(new DataGridViewTextBoxColumn() {Name="Schedule", HeaderText = "Schedule" });
+            dgv.Columns.Add(new DataGridViewTextBoxColumn() { Name = "CollectionType", HeaderText = "Collection Type", ReadOnly = true, AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill });
+            dgv.Columns.Add(new DataGridViewTextBoxColumn() {Name="Schedule", HeaderText = "Schedule", ToolTipText="Cron expression or time in seconds" });
             dgv.Columns.Add(new DataGridViewTextBoxColumn() {Name="ScheduleDescription", HeaderText = "Schedule Description", ReadOnly = true });
             dgv.Columns.Add(new DataGridViewCheckBoxColumn() {Name="RunOnStart", HeaderText = "Run on service start" });
-            dgv.Columns.Add(new DataGridViewCheckBoxColumn() {Name="Default", HeaderText = "Default" });
+            dgv.Columns.Add(new DataGridViewCheckBoxColumn() {Name="Default", HeaderText = "Default", ToolTipText = "Uncheck to supply a custom cron schedule or check to use the default schedule."});
 
             if (userSchedule != null)
             {
                 foreach (var s in userSchedule)
                 {
-                    int idx = dgv.Rows.Add(new object[] { Enum.GetName(typeof(CollectionType), s.Key), s.Value.Schedule, getScheduleDescription(s.Value.Schedule), s.Value.RunOnServiceStart, false });
-                    formatRow(idx);
+                    int idx = dgv.Rows.Add(new object[] { Enum.GetName(typeof(CollectionType), s.Key), s.Value.Schedule, GetScheduleDescription(s.Value.Schedule), s.Value.RunOnServiceStart, false });
+                    FormatRow(idx);
                 }
             }
             foreach (var s in BaseSchedule)
             {
                 if (userSchedule==null || !userSchedule.ContainsKey(s.Key))
                 {
-                    int idx = dgv.Rows.Add(new object[] { Enum.GetName(typeof(CollectionType), s.Key), s.Value.Schedule, getScheduleDescription(s.Value.Schedule), s.Value.RunOnServiceStart, true });
-                    formatRow(idx);
+                    int idx = dgv.Rows.Add(new object[] { Enum.GetName(typeof(CollectionType), s.Key), s.Value.Schedule, GetScheduleDescription(s.Value.Schedule), s.Value.RunOnServiceStart, true });
+                    FormatRow(idx);
                 }
             }
             dgv.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
         }
 
-        private string getScheduleDescription(string schedule)
+        private static string GetScheduleDescription(string schedule)
         {
             if (string.IsNullOrEmpty(schedule))
             {
@@ -97,13 +104,13 @@ namespace DBADashServiceConfig
             }
         }
 
-        private void dgv_RowValidating(object sender, DataGridViewCellCancelEventArgs e)
+        private void Dgv_RowValidating(object sender, DataGridViewCellCancelEventArgs e)
         {
             string schedule = (string)dgv[1,e.RowIndex].Value;
             
             try
             {                    
-                dgv[2, e.RowIndex].Value =getScheduleDescription(schedule);              
+                dgv[2, e.RowIndex].Value = GetScheduleDescription(schedule);              
             }
             catch
             {
@@ -112,12 +119,12 @@ namespace DBADashServiceConfig
             }
         }
 
-        private void dgv_CellValidated(object sender, DataGridViewCellEventArgs e)
+        private void Dgv_CellValidated(object sender, DataGridViewCellEventArgs e)
         {
 
         }
 
-        private void dgv_RowsAdded(object sender, DataGridViewRowsAddedEventArgs e) { 
+        private void Dgv_RowsAdded(object sender, DataGridViewRowsAddedEventArgs e) { 
        
             for (int i= 0; i < e.RowCount - 1; i++){
                 dgv.Rows[e.RowIndex + i].Cells["Schedule"].ReadOnly = (bool)dgv.Rows[e.RowIndex + i].Cells["Default"].Value;
@@ -125,16 +132,16 @@ namespace DBADashServiceConfig
             }
         }
 
-        private void dgv_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        private void Dgv_CellValueChanged(object sender, DataGridViewCellEventArgs e)
         {
             if(e.ColumnIndex == dgv.Columns["Default"].Index)
             {
-                formatRow(e.RowIndex);
+                FormatRow(e.RowIndex);
             }
   
         }
 
-        private void formatRow(int idx)
+        private void FormatRow(int idx)
         {
             var row = dgv.Rows[idx];
             bool isDefault = (bool)row.Cells["Default"].Value;
@@ -149,33 +156,86 @@ namespace DBADashServiceConfig
                 CollectionType collectType = (CollectionType)Enum.Parse(typeof(CollectionType), (string)row.Cells["CollectionType"].Value);
                 row.Cells["Schedule"].Value = BaseSchedule[collectType].Schedule;
                 row.Cells["RunOnStart"].Value = BaseSchedule[collectType].RunOnServiceStart;
-                row.Cells["ScheduleDescription"].Value = getScheduleDescription(BaseSchedule[collectType].Schedule);
-            }
+            }      
+             row.Cells["ScheduleDescription"].Value = GetScheduleDescription(Convert.ToString(row.Cells["Schedule"].Value));
+            
         }
 
-        private void dgv_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void Dgv_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             if (e.ColumnIndex == dgv.Columns["Default"].Index)
             {
                 dgv.EndEdit();
-                formatRow(e.RowIndex);
+                FormatRow(e.RowIndex);
             }
         }
 
-         private void bttnCancel_Click(object sender, EventArgs e)
+         private void BttnCancel_Click(object sender, EventArgs e)
         {
             this.DialogResult = DialogResult.Cancel;
         }
 
-        private void bttnOK_Click(object sender, EventArgs e)
+        private void BttnOK_Click(object sender, EventArgs e)
         {
             this.DialogResult = DialogResult.OK;
         }
 
-        private void lnkCron_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        private void LnkCron_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             var psi = new ProcessStartInfo("https://github.com/trimble-oss/dba-dash/blob/main/Docs/Collection.md#cron-expressions") { UseShellExecute = true };
             Process.Start(psi);
+        }
+
+        private void Dgv_SelectionChanged(object sender, EventArgs e)
+        {
+            tsCopy.Enabled = dgv.SelectedRows.Count == 1;
+            tsPaste.Enabled = hasCopy && dgv.SelectedRows.Count > 0;
+            tsSetDefault.Enabled = dgv.SelectedRows.Count > 0;
+            tsDisable.Enabled = dgv.SelectedRows.Count > 0;
+        }
+
+        private void TsPaste_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dgv.SelectedRows)
+            {
+                row.Cells["Default"].Value = copyDefault;
+                row.Cells["RunOnStart"].Value = copyRunOnStart;
+                row.Cells["Schedule"].Value = copySchedule;
+                FormatRow(row.Index);
+            }
+        }
+
+        private void TsCopy_Click(object sender, EventArgs e)
+        {
+            if (dgv.SelectedRows.Count == 1)
+            {
+                var row = dgv.SelectedRows[0];
+                copySchedule = Convert.ToString(row.Cells["Schedule"].Value);
+                copyDefault = Convert.ToBoolean(row.Cells["Default"].Value);
+                copyRunOnStart = Convert.ToBoolean(row.Cells["RunOnStart"].Value);
+                hasCopy = true;
+                tsPaste.Enabled = true;
+            }
+        }
+
+        private void TsSetDefault_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dgv.SelectedRows)
+            {
+                row.Cells["Default"].Value = true;
+                FormatRow(row.Index);
+            }
+        }
+
+        private void TsDisable_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dgv.SelectedRows)
+            {
+                row.Cells["Default"].Value = false;
+                row.Cells["RunOnStart"].Value = false;
+                row.Cells["Schedule"].Value = "";
+                FormatRow(row.Index);
+            }
         }
     }
 }
