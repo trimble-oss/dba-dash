28fd060046c12717448cabec1062e24fde790f3f
David Wiseman <wiseman1982@googlemail.com>
2022-09-24 15:52:38 +0100

    Handle mouse back button on Log Shipping tab
    Use Mouse back button to trigger the back button on the toolbar.
    
    Fix Navigate back button

--- DBADashGUI/HA/LogShippingControl.cs
+++ DBADashGUI/HA/LogShippingControl.cs
@@ -74,6 +74,39 @@ namespace DBADashGUI.LogShipping
             }
         }
 
+        private void HandlePreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.XButton1:
+                    NavigateBack();
+                    break;
+            }
+        }
+        private void HandleMouseDown(object sender, MouseEventArgs e)
+        {
+            switch (e.Button)
+            {
+                case MouseButtons.XButton1:
+                    NavigateBack();
+                    break;
+            }
+        }
+        // https://stackoverflow.com/questions/41637248/how-do-i-capture-mouse-back-button-and-cause-it-to-do-something-else
+        private void HookupNavigationButtons(Control ctrl)
+        {
+            for (int t = ctrl.Controls.Count - 1; t >= 0; t--)
+            {
+                Control c = ctrl.Controls[t];
+                c.PreviewKeyDown -= HandlePreviewKeyDown;
+                c.PreviewKeyDown += HandlePreviewKeyDown;
+                c.MouseDown -= HandleMouseDown;
+                c.MouseDown += HandleMouseDown;
+                HookupNavigationButtons(c);
+            }
+        }
+
+
         private void refreshSummary()
         {
             using (var cn = new SqlConnection(Common.ConnectionString))
@@ -141,6 +174,7 @@ namespace DBADashGUI.LogShipping
         {
             InitializeComponent();
             Common.StyleGrid(ref dgvLogShipping);
+            HookupNavigationButtons(this); // Handle mouse back button
         }
 
         private void tsFilter_Click(object sender, EventArgs e)
@@ -251,12 +285,20 @@ namespace DBADashGUI.LogShipping
 
         private void tsBack_Click(object sender, EventArgs e)
         {
-            instanceIDs = cachedInstanceIDs;
-            IncludeCritical = true;
-            IncludeNA = false;
-            IncludeOK =false;
-            IncludeWarning = true;
-            RefreshData();
+            NavigateBack();
+        }
+
+        private void NavigateBack()
+        {
+            if (tsBack.Enabled)
+            {
+                instanceIDs = cachedInstanceIDs;
+                IncludeCritical = true;
+                IncludeNA = false;
+                IncludeOK = false;
+                IncludeWarning = true;
+                RefreshData();
+            }
         }
 
         private void dgvSummary_RowsAdded(object sender, DataGridViewRowsAddedEventArgs e)
