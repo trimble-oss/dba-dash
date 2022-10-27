7e593a572c7068d54ebdc9c50c680805dc1c860c
David Wiseman <wiseman1982@googlemail.com>
2022-10-27 20:03:24 +0100

    Package Serialized DataSet Viewer

---
+++ DBADashServiceConfig/PostBuild.ps1
@@ -0,0 +1,17 @@
+﻿param([string]$TargetDir);
+$ErrorActionPreference = "Stop"
+$URL = "https://github.com/trimble-oss/serialized-dataset-viewer/releases/download/1.1.0/SerializedDataSetViewer_1.1.0.zip"
+$FileName = [System.IO.Path]::GetFileName($URL)
+
+$DownloadPath = [System.IO.Path]::Combine($TargetDir, $FileName)
+$Folder = [System.IO.Path]::Combine($TargetDir,"SerializedDataSetViewer")
+$ConfigFile = [System.IO.Path]::Combine($Folder,"appsettings.json")
+
+Invoke-WebRequest $URL -Out $DownloadPath
+
+Expand-Archive -Path $DownloadPath -DestinationPath $Folder -Force -ErrorAction Stop
+
+Remove-Item -Path $DownloadPath
+
+$ConfigFile
+Set-Content -Path $ConfigFile -Value '{ "DefaultPath": "..\\Failed" }'
