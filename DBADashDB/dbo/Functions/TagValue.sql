c4784451d72b46fee1fce28af71f01a46d476ca9
David Wiseman <dave_wiseman@trimble.com>
2022-11-04 08:43:59 +0000

    Fix grouping by system tag for Azure DB
    #428

--- DBADashDB/dbo/Functions/TagValue.sql
+++ DBADashDB/dbo/Functions/TagValue.sql
@@ -1,6 +1,7 @@
 ﻿/* 
 	Returns tag value(s) associated with a specified tag for an instance.
-	Note: Can ruturn zero rows or more than 1 row
+	Note: Can return zero rows or more than 1 row.  
+	e.g. An instance can be tagged Product:ABC and Product:XYZ
 */
 CREATE FUNCTION dbo.TagValue(
 	@InstanceID INT,
@@ -10,18 +11,24 @@ CREATE FUNCTION dbo.TagValue(
 RETURNS TABLE 
 AS
 RETURN
+/* Tags associated with the InstanceID.  System tags and user tags for regular instances */
 SELECT  T.TagValue
 FROM dbo.InstanceIDsTags IT 
 JOIN dbo.Tags T ON IT.TagID = T.TagID
 WHERE InstanceID = @InstanceID
 AND T.TagName = @TagName
-AND @EngineEdition <> 5 -- AzureDB
+AND (@EngineEdition <> 5 OR @TagName LIKE '{%') /* System tags only for AzureDB */
 UNION ALL
+/* 
+Tags associated with the Instance name.  These are user defined tags for AzureDB.
+System tags for AzureDB are associated with InstanceID. (Some tags like CPU count can vary by database)
+*/
 SELECT T.TagValue
 FROM dbo.Instances I
 JOIN dbo.InstanceTags IT ON IT.Instance = I.Instance
 JOIN dbo.Tags T ON IT.TagID = T.TagID 
 WHERE I.InstanceID = @InstanceID
-AND I.EngineEdition = 5 -- AzureDB
+AND I.EngineEdition = 5 /* AzureDB */
 AND T.TagName= @TagName
-
+AND T.TagName NOT LIKE '{%' /* User tags only */
+GO
