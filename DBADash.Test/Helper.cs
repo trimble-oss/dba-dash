9970c7bb82597e7c86a945b3eefbc608b1c47c6b
David Wiseman <dave_wiseman@trimble.com>
2022-11-10 09:58:16 +0000

    Update NuGet packages

--- DBADash.Test/Helper.cs
+++ DBADash.Test/Helper.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DBADashConfig.Test
 {
