trimble-oss/dba-dash

commit 50007560051633a9d2db71aee996d331e0d11680

Fix Invalid column name 'contained_availability_group_id' error
The error occurs on GitHub actions but not in the lab environment.  Use dynamic SQL to ensure the statement isn't parsed unless we are on SQL 2022.

DBADash/SQL/SQLInstance.sql
@@ -4,11 +4,14 @@ DECLARE @ContainedAGID UNIQUEIDENTIFIER
 /* For SQL 2022+ Get the Contained Availability Group Name */
 IF COLUMNPROPERTY(OBJECT_ID('sys.dm_exec_sessions'),'contained_availability_group_id','ColumnID') IS NOT NULL
 BEGIN
+	DECLARE @SQL NVARCHAR(MAX)
+	SET @SQL = N'
 	SELECT	@ContainedAGName = AG.name,
 			@ContainedAGID = S.contained_availability_group_id 
 	FROM sys.dm_exec_sessions S 
 	JOIN sys.availability_groups AG ON S.contained_availability_group_id = AG.group_id
-	WHERE session_id = @@SPID
+	WHERE session_id = @@SPID'
+	EXEC sp_executesql @SQL,N'@ContainedAGName SYSNAME OUT,@ContainedAGID UNIQUEIDENTIFIER OUT',@ContainedAGName OUT,@ContainedAGID OUT
 END 
 IF OBJECT_ID('sys.dm_os_host_info') IS NOT NULL
 BEGIN
@@ -70,4 +73,3 @@ BEGIN
 		@ContainedAGID as contained_availability_group_id,
 		@ContainedAGName AS contained_availability_group_name
 END
-

commit eb23eaf9537bb0d9c51770dc2d24bf49eef4501a

AzureDB collection fix
Fix issue for Azure DB collections caused by PR #445

DBADash/SQL/SQLInstance.sql
@@ -3,6 +3,7 @@ DECLARE @ContainedAGID UNIQUEIDENTIFIER
 
 /* For SQL 2022+ Get the Contained Availability Group Name */
 IF COLUMNPROPERTY(OBJECT_ID('sys.dm_exec_sessions'),'contained_availability_group_id','ColumnID') IS NOT NULL
+	AND OBJECT_ID('sys.availability_groups') IS NOT NULL /* AzureDB has contained_availability_group_id column but not sys.availability_groups DMV */
 BEGIN
 	DECLARE @SQL NVARCHAR(MAX)
 	SET @SQL = N'