8084f83d034f1b4a3f1fd5fb657d204fa58e5412
David Wiseman <wiseman1982@googlemail.com>
2022-12-05 21:18:16 +0000

    Add option to save time zone selection
    #461

--- DBADashGUI/DBADashUser.cs
+++ DBADashGUI/DBADashUser.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Data.SqlClient;
 using System;
 using System.Data;
+using System.Windows;
 
 namespace DBADashGUI
 {
@@ -13,6 +14,25 @@ namespace DBADashGUI
 
         public static bool HasManageGlobalViews = false;
 
+        public static void SetUserTimeZone(TimeZoneInfo timeZone)
+        {
+            using (var cn = new SqlConnection(Common.ConnectionString))
+            using (var cmd = new SqlCommand("DBADash.User_TimeZone_Upd", cn) { CommandType = CommandType.StoredProcedure })
+            {
+                cn.Open();
+                cmd.Parameters.AddWithValue("UserID", UserID);
+                if (timeZone == TimeZoneInfo.Local)
+                {
+                    cmd.Parameters.AddWithValue("TimeZone", DBNull.Value);
+                }
+                else
+                {
+                    cmd.Parameters.AddWithValue("TimeZone", timeZone.Id);
+                }
+                cmd.ExecuteNonQuery();
+            }
+        }
+
         public static void GetUser()
         {
             using (var cn = new SqlConnection(Common.ConnectionString))
@@ -22,14 +42,29 @@ namespace DBADashGUI
                 cmd.Parameters.AddWithValue("UserName", Environment.UserName);
                 var pUserID = new SqlParameter("UserID", SqlDbType.Int) { Direction = ParameterDirection.Output };
                 var pManageGlobalViews = new SqlParameter("ManageGlobalViews", SqlDbType.Bit) { Direction = ParameterDirection.Output };
+                var pTZ = new SqlParameter("TimeZone", SqlDbType.VarChar, 50) { Direction = ParameterDirection.Output };
                 cmd.Parameters.Add(pUserID);
                 cmd.Parameters.Add(pManageGlobalViews);
+                cmd.Parameters.Add(pTZ);
                 cmd.ExecuteNonQuery();
                 int id = Convert.ToInt32(pUserID.Value);
                 if (id > 0)
                 {
                     _UserID = (int)pUserID.Value;
                     HasManageGlobalViews = (bool)pManageGlobalViews.Value;
+                    if (pTZ.Value != DBNull.Value)
+                    {
+                        string tzID = (string)pTZ.Value;
+                        try
+                        {
+                            DateHelper.AppTimeZone = TimeZoneInfo.FindSystemTimeZoneById(tzID);
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Time zone not found " + tzID, "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                            DateHelper.AppTimeZone = TimeZoneInfo.Local;
+                        }
+                    }
                 }
                 else
                 {
