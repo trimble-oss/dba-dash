eb07f9c4b16b9b0885c974e9a292aebd518cae9e
David Wiseman <wiseman1982@googlemail.com>
2023-02-15 22:08:31 +0000

    Query timeout increase for DB level collections
    #535

--- DBADash/CollectionCommandTimeout.cs
+++ DBADash/CollectionCommandTimeout.cs
@@ -5,12 +5,13 @@ namespace DBADash
     internal static class CollectionCommandTimeout
     {
         private static readonly Dictionary<CollectionType, int> collectionCommandTimeouts = new() {
-            { CollectionType.DatabasePermissions,300 },
-            { CollectionType.DatabasePrincipals,300 },
-            { CollectionType.DatabaseRoleMembers,300 },
-            { CollectionType.IdentityColumns,300 },
+            { CollectionType.DatabasePermissions,900 },
+            { CollectionType.DatabasePrincipals,900 },
+            { CollectionType.DatabaseRoleMembers,900 },
+            { CollectionType.IdentityColumns,900 },
             { CollectionType.SlowQueries, 90 }
         };
+
         public static int DefaultCommandTimeout = 60;
 
         public static int GetCommandTimeout(this CollectionType type)
