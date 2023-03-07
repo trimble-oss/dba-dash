17507093e23822bbf44bf82e08d35f6541267eb7
David Wiseman <wiseman1982@googlemail.com>
2023-03-07 20:45:45 +0000

    Fix SSDT build issue with msbuild 17.5
    Fix issue where msbuild 17.5 fails with error 71501.  Move PatritionBoundaryHelper & PartitionFunctionName functions to post-deployment.  Add suppressions for 71502 where needed.
    Remove sys.types from IdentityColumnsInfo view.
    Add missing ORDER BY to IdentityColumnsInfo.
    #554

--- DBADashDB/dbo/Functions/PartitionBoundaryHelper.sql
+++ DBADashDB/dbo/Functions/PartitionBoundaryHelper.sql
@@ -1,8 +1,16 @@
-﻿CREATE FUNCTION PartitionBoundaryHelper(@PartitionFunction SYSNAME,@TableName SYSNAME)
+﻿/* 
+	Created in post deployment. See: https://github.com/trimble-oss/dba-dash/issues/554
+*/
+IF OBJECT_ID('dbo.PartitionBoundaryHelper') IS NULL
+BEGIN
+	EXEC sp_executesql N'CREATE FUNCTION dbo.PartitionBoundaryHelper() RETURNS TABLE AS RETURN SELECT 1 a'
+END
+GO
+ALTER FUNCTION dbo.PartitionBoundaryHelper(@PartitionFunction SYSNAME,@TableName SYSNAME)
 RETURNS TABLE
 AS
 RETURN
-SELECT CAST(lbprv.value AS datetime) lb,CAST(ubprv.value AS datetime) ub,p.partition_number,p.rows
+SELECT CAST(lbprv.value AS DATETIME) lb,CAST(ubprv.value AS DATETIME) ub,p.partition_number,p.rows
 FROM sys.partitions p
 LEFT JOIN sys.partition_range_values lbprv 
 	INNER JOIN sys.partition_functions AS lbpf ON  lbpf.function_id = lbprv.function_id 
@@ -14,3 +22,4 @@ LEFT JOIN sys.partition_range_values ubprv
 									ON p.partition_number = ubprv.boundary_id
 where p.object_id = object_id(@TableName)
 AND p.index_id = 1
+GO
