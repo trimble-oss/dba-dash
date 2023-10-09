4b794fbfb433862b836ffa14a071a3c1a8df0143
David Wiseman <33157668+DavidWiseman@users.noreply.github.com>
2023-10-09 11:11:45 +0100

    Human Duration functions updated to return blank string on NULL value
    #719

--- DBADashDB/dbo/Functions/SecondsToHumanDuration.sql
+++ DBADashDB/dbo/Functions/SecondsToHumanDuration.sql
@@ -3,7 +3,7 @@
 )
 RETURNS TABLE
 AS
-RETURN SELECT CASE WHEN ABS(@Seconds)>=8640000 THEN CONCAT(@Seconds/86400,' Days') ELSE
+RETURN SELECT CASE WHEN @Seconds IS NULL THEN '' WHEN ABS(@Seconds)>=8640000 THEN CONCAT(@Seconds/86400,' Days') ELSE
 				CONCAT(
 						CASE WHEN @Seconds<0 THEN '-' ELSE '' END,
                         RIGHT('00' + CAST(ISNULL((ABS(@Seconds) / 3600 / 24), 0) AS VARCHAR(2)), 2), ' ',
