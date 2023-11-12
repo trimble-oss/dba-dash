d7c04ff34b5445a1d5129c76a5a4c19d4665d15b
David Wiseman <wiseman1982@googlemail.com>
2023-11-12 20:12:30 +0000

    Configure Data Retention in GUI for custom collections
    Pass SchemaName to DataRetention_Upd.

--- DBADashGUI/Options Menu/DataRetention.cs
+++ DBADashGUI/Options Menu/DataRetention.cs
@@ -36,16 +36,14 @@ namespace DBADashGUI
 
         private static DataTable GetDataRetention(bool allTables)
         {
-            using (var cn = new SqlConnection(Common.ConnectionString))
-            using (var cmd = new SqlCommand("dbo.DataRetention_Get", cn) { CommandType = CommandType.StoredProcedure })
-            using (var da = new SqlDataAdapter(cmd))
-            {
-                cn.Open();
-                cmd.Parameters.AddWithValue("AllTables", allTables);
-                DataTable dt = new();
-                da.Fill(dt);
-                return dt;
-            }
+            using var cn = new SqlConnection(Common.ConnectionString);
+            using var cmd = new SqlCommand("dbo.DataRetention_Get", cn) { CommandType = CommandType.StoredProcedure };
+            using var da = new SqlDataAdapter(cmd);
+            cn.Open();
+            cmd.Parameters.AddWithValue("AllTables", allTables);
+            DataTable dt = new();
+            da.Fill(dt);
+            return dt;
         }
 
         private void ShowAllTablesToolStripMenuItem_Click(object sender, EventArgs e)
@@ -55,46 +53,40 @@ namespace DBADashGUI
 
         private void Dgv_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            if (e.RowIndex >= 0 && dgv.Columns[e.ColumnIndex].Name == "colRetentionDays")
+            if (e.RowIndex < 0 || dgv.Columns[e.ColumnIndex].Name != "colRetentionDays") return;
+            var days = (int)dgv[e.ColumnIndex, e.RowIndex].Value;
+            var sDays = days.ToString();
+            var tableName = (string)dgv["colTableName", e.RowIndex].Value;
+            var schema = (string)dgv["colSchema", e.RowIndex].Value;
+            if (CommonShared.ShowInputDialog(ref sDays, "Enter number of days") != DialogResult.OK) return;
+            if (int.TryParse(sDays, out var newRetention))
             {
-                int days = (int)dgv[e.ColumnIndex, e.RowIndex].Value;
-                string sDays = days.ToString();
-                string tableName = (string)dgv["colTableName", e.RowIndex].Value;
-                if (Common.ShowInputDialog(ref sDays, "Enter number of days") == DialogResult.OK)
+                if (newRetention == days) return;
+                try
                 {
-                    if (int.TryParse(sDays, out int newRetention))
-                    {
-                        if (newRetention != days)
-                        {
-                            try
-                            {
-                                UpdateRetention(tableName, newRetention);
-                                dgv[e.ColumnIndex, e.RowIndex].Value = newRetention;
-                            }
-                            catch (Exception ex)
-                            {
-                                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                            }
-                        }
-                    }
-                    else
-                    {
-                        MessageBox.Show("Please enter an integer number of days for retention", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    }
+                    UpdateRetention(tableName, schema, newRetention);
+                    dgv[e.ColumnIndex, e.RowIndex].Value = newRetention;
                 }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter an integer number of days for retention", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
-        private static void UpdateRetention(string table, int days)
+        private static void UpdateRetention(string table, string schema, int days)
         {
-            using (var cn = new SqlConnection(Common.ConnectionString))
-            using (SqlCommand cmd = new("dbo.DataRetention_Upd", cn) { CommandType = CommandType.StoredProcedure })
-            {
-                cn.Open();
-                cmd.Parameters.AddWithValue("TableName", table);
-                cmd.Parameters.AddWithValue("RetentionDays", days);
-                cmd.ExecuteNonQuery();
-            }
+            using var cn = new SqlConnection(Common.ConnectionString);
+            using SqlCommand cmd = new("dbo.DataRetention_Upd", cn) { CommandType = CommandType.StoredProcedure };
+            cn.Open();
+            cmd.Parameters.AddWithValue("TableName", table);
+            cmd.Parameters.AddWithValue("SchemaName", schema);
+            cmd.Parameters.AddWithValue("RetentionDays", days);
+            cmd.ExecuteNonQuery();
         }
 
         private void TsRefresh_Click(object sender, EventArgs e)
