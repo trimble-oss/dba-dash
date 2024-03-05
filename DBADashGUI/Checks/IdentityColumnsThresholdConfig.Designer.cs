60137c0eff9c7b29f9ca8c28cd49d9f555dacd28
David Wiseman <33157668+DavidWiseman@users.noreply.github.com>
2024-03-05 10:45:38 +0000

    Identity Column Threshold Configuration Fix
    Move inherit checkbox so it's always visible.  Allow extra space for text and allow it to wrap to next line.
    #844

--- DBADashGUI/Checks/IdentityColumnsThresholdConfig.Designer.cs
+++ DBADashGUI/Checks/IdentityColumnsThresholdConfig.Designer.cs
@@ -28,171 +28,169 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.numWarning = new System.Windows.Forms.NumericUpDown();
-            this.numCritical = new System.Windows.Forms.NumericUpDown();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.bttnUpdate = new System.Windows.Forms.Button();
-            this.bttnCancel = new System.Windows.Forms.Button();
-            this.chkInherit = new System.Windows.Forms.CheckBox();
-            this.chkWarning = new System.Windows.Forms.CheckBox();
-            this.chkCritical = new System.Windows.Forms.CheckBox();
-            this.lblObject = new System.Windows.Forms.Label();
-            this.pnlConfig = new System.Windows.Forms.Panel();
-            this.bttnUp = new System.Windows.Forms.Button();
-            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.numWarning)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numCritical)).BeginInit();
-            this.pnlConfig.SuspendLayout();
-            this.SuspendLayout();
+            components = new System.ComponentModel.Container();
+            numWarning = new System.Windows.Forms.NumericUpDown();
+            numCritical = new System.Windows.Forms.NumericUpDown();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            bttnUpdate = new System.Windows.Forms.Button();
+            bttnCancel = new System.Windows.Forms.Button();
+            chkInherit = new System.Windows.Forms.CheckBox();
+            chkWarning = new System.Windows.Forms.CheckBox();
+            chkCritical = new System.Windows.Forms.CheckBox();
+            lblObject = new System.Windows.Forms.Label();
+            pnlConfig = new System.Windows.Forms.Panel();
+            bttnUp = new System.Windows.Forms.Button();
+            toolTip1 = new System.Windows.Forms.ToolTip(components);
+            ((System.ComponentModel.ISupportInitialize)numWarning).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numCritical).BeginInit();
+            pnlConfig.SuspendLayout();
+            SuspendLayout();
             // 
             // numWarning
             // 
-            this.numWarning.Location = new System.Drawing.Point(174, 7);
-            this.numWarning.Name = "numWarning";
-            this.numWarning.Size = new System.Drawing.Size(150, 27);
-            this.numWarning.TabIndex = 0;
+            numWarning.Location = new System.Drawing.Point(245, 8);
+            numWarning.Name = "numWarning";
+            numWarning.Size = new System.Drawing.Size(152, 27);
+            numWarning.TabIndex = 0;
             // 
             // numCritical
             // 
-            this.numCritical.Location = new System.Drawing.Point(174, 51);
-            this.numCritical.Name = "numCritical";
-            this.numCritical.Size = new System.Drawing.Size(150, 27);
-            this.numCritical.TabIndex = 1;
+            numCritical.Location = new System.Drawing.Point(245, 52);
+            numCritical.Name = "numCritical";
+            numCritical.Size = new System.Drawing.Size(152, 27);
+            numCritical.TabIndex = 1;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(149, 20);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Warning Threshold %";
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(10, 11);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(149, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Warning Threshold %";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 53);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(140, 20);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Critical Threshold %";
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(10, 55);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(140, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Critical Threshold %";
             // 
             // bttnUpdate
             // 
-            this.bttnUpdate.Location = new System.Drawing.Point(311, 153);
-            this.bttnUpdate.Name = "bttnUpdate";
-            this.bttnUpdate.Size = new System.Drawing.Size(94, 29);
-            this.bttnUpdate.TabIndex = 4;
-            this.bttnUpdate.Text = "&Update";
-            this.bttnUpdate.UseVisualStyleBackColor = true;
-            this.bttnUpdate.Click += new System.EventHandler(this.BttnUpdate_Click);
+            bttnUpdate.Location = new System.Drawing.Point(329, 150);
+            bttnUpdate.Name = "bttnUpdate";
+            bttnUpdate.Size = new System.Drawing.Size(94, 29);
+            bttnUpdate.TabIndex = 4;
+            bttnUpdate.Text = "&Update";
+            bttnUpdate.UseVisualStyleBackColor = true;
+            bttnUpdate.Click += BttnUpdate_Click;
             // 
             // bttnCancel
             // 
-            this.bttnCancel.Location = new System.Drawing.Point(211, 153);
-            this.bttnCancel.Name = "bttnCancel";
-            this.bttnCancel.Size = new System.Drawing.Size(94, 29);
-            this.bttnCancel.TabIndex = 5;
-            this.bttnCancel.Text = "&Cancel";
-            this.bttnCancel.UseVisualStyleBackColor = true;
-            this.bttnCancel.Click += new System.EventHandler(this.BttnCancel_Click);
+            bttnCancel.Location = new System.Drawing.Point(187, 150);
+            bttnCancel.Name = "bttnCancel";
+            bttnCancel.Size = new System.Drawing.Size(94, 29);
+            bttnCancel.TabIndex = 5;
+            bttnCancel.Text = "&Cancel";
+            bttnCancel.UseVisualStyleBackColor = true;
+            bttnCancel.Click += BttnCancel_Click;
             // 
             // chkInherit
             // 
-            this.chkInherit.AutoSize = true;
-            this.chkInherit.Enabled = false;
-            this.chkInherit.Location = new System.Drawing.Point(342, 12);
-            this.chkInherit.Name = "chkInherit";
-            this.chkInherit.Size = new System.Drawing.Size(73, 24);
-            this.chkInherit.TabIndex = 6;
-            this.chkInherit.Text = "Inherit";
-            this.chkInherit.UseVisualStyleBackColor = true;
-            this.chkInherit.CheckedChanged += new System.EventHandler(this.ChkInherit_CheckedChanged);
+            chkInherit.AutoSize = true;
+            chkInherit.Enabled = false;
+            chkInherit.Location = new System.Drawing.Point(15, 153);
+            chkInherit.Name = "chkInherit";
+            chkInherit.Size = new System.Drawing.Size(73, 24);
+            chkInherit.TabIndex = 6;
+            chkInherit.Text = "Inherit";
+            chkInherit.UseVisualStyleBackColor = true;
+            chkInherit.CheckedChanged += ChkInherit_CheckedChanged;
             // 
             // chkWarning
             // 
-            this.chkWarning.AutoSize = true;
-            this.chkWarning.Checked = true;
-            this.chkWarning.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.chkWarning.Location = new System.Drawing.Point(330, 12);
-            this.chkWarning.Name = "chkWarning";
-            this.chkWarning.Size = new System.Drawing.Size(18, 17);
-            this.chkWarning.TabIndex = 7;
-            this.toolTip1.SetToolTip(this.chkWarning, "Enable/Disable check");
-            this.chkWarning.UseVisualStyleBackColor = true;
-            this.chkWarning.CheckedChanged += new System.EventHandler(this.ChkWarning_CheckedChanged);
+            chkWarning.AutoSize = true;
+            chkWarning.Checked = true;
+            chkWarning.CheckState = System.Windows.Forms.CheckState.Checked;
+            chkWarning.Location = new System.Drawing.Point(403, 13);
+            chkWarning.Name = "chkWarning";
+            chkWarning.Size = new System.Drawing.Size(18, 17);
+            chkWarning.TabIndex = 7;
+            toolTip1.SetToolTip(chkWarning, "Enable/Disable check");
+            chkWarning.UseVisualStyleBackColor = true;
+            chkWarning.CheckedChanged += ChkWarning_CheckedChanged;
             // 
             // chkCritical
             // 
-            this.chkCritical.AutoSize = true;
-            this.chkCritical.Checked = true;
-            this.chkCritical.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.chkCritical.Location = new System.Drawing.Point(330, 56);
-            this.chkCritical.Name = "chkCritical";
-            this.chkCritical.Size = new System.Drawing.Size(18, 17);
-            this.chkCritical.TabIndex = 8;
-            this.toolTip1.SetToolTip(this.chkCritical, "Enable/Disable check");
-            this.chkCritical.UseVisualStyleBackColor = true;
-            this.chkCritical.CheckedChanged += new System.EventHandler(this.ChkCritical_CheckedChanged);
+            chkCritical.AutoSize = true;
+            chkCritical.Checked = true;
+            chkCritical.CheckState = System.Windows.Forms.CheckState.Checked;
+            chkCritical.Location = new System.Drawing.Point(403, 57);
+            chkCritical.Name = "chkCritical";
+            chkCritical.Size = new System.Drawing.Size(18, 17);
+            chkCritical.TabIndex = 8;
+            toolTip1.SetToolTip(chkCritical, "Enable/Disable check");
+            chkCritical.UseVisualStyleBackColor = true;
+            chkCritical.CheckedChanged += ChkCritical_CheckedChanged;
             // 
             // lblObject
             // 
-            this.lblObject.AutoSize = true;
-            this.lblObject.Location = new System.Drawing.Point(49, 13);
-            this.lblObject.Name = "lblObject";
-            this.lblObject.Size = new System.Drawing.Size(51, 20);
-            this.lblObject.TabIndex = 9;
-            this.lblObject.Text = "{Root}";
+            lblObject.Location = new System.Drawing.Point(49, 7);
+            lblObject.Name = "lblObject";
+            lblObject.Size = new System.Drawing.Size(383, 42);
+            lblObject.TabIndex = 9;
+            lblObject.Text = "{Root}";
             // 
             // pnlConfig
             // 
-            this.pnlConfig.Controls.Add(this.label1);
-            this.pnlConfig.Controls.Add(this.numWarning);
-            this.pnlConfig.Controls.Add(this.chkCritical);
-            this.pnlConfig.Controls.Add(this.numCritical);
-            this.pnlConfig.Controls.Add(this.chkWarning);
-            this.pnlConfig.Controls.Add(this.label2);
-            this.pnlConfig.Location = new System.Drawing.Point(12, 36);
-            this.pnlConfig.Name = "pnlConfig";
-            this.pnlConfig.Size = new System.Drawing.Size(360, 98);
-            this.pnlConfig.TabIndex = 10;
+            pnlConfig.Controls.Add(label1);
+            pnlConfig.Controls.Add(numWarning);
+            pnlConfig.Controls.Add(chkCritical);
+            pnlConfig.Controls.Add(numCritical);
+            pnlConfig.Controls.Add(chkWarning);
+            pnlConfig.Controls.Add(label2);
+            pnlConfig.Location = new System.Drawing.Point(2, 52);
+            pnlConfig.Name = "pnlConfig";
+            pnlConfig.Size = new System.Drawing.Size(439, 88);
+            pnlConfig.TabIndex = 10;
             // 
             // bttnUp
             // 
-            this.bttnUp.Image = global::DBADashGUI.Properties.Resources.arrow_Up_16xLG;
-            this.bttnUp.Location = new System.Drawing.Point(12, 7);
-            this.bttnUp.Name = "bttnUp";
-            this.bttnUp.Size = new System.Drawing.Size(31, 29);
-            this.bttnUp.TabIndex = 11;
-            this.toolTip1.SetToolTip(this.bttnUp, "Up Level");
-            this.bttnUp.UseVisualStyleBackColor = true;
-            this.bttnUp.Click += new System.EventHandler(this.BttnUp_Click);
+            bttnUp.Image = Properties.Resources.arrow_Up_16xLG;
+            bttnUp.Location = new System.Drawing.Point(12, 7);
+            bttnUp.Name = "bttnUp";
+            bttnUp.Size = new System.Drawing.Size(31, 29);
+            bttnUp.TabIndex = 11;
+            toolTip1.SetToolTip(bttnUp, "Up Level");
+            bttnUp.UseVisualStyleBackColor = true;
+            bttnUp.Click += BttnUp_Click;
             // 
             // IdentityColumnsThresholdConfig
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(417, 202);
-            this.Controls.Add(this.bttnUp);
-            this.Controls.Add(this.pnlConfig);
-            this.Controls.Add(this.lblObject);
-            this.Controls.Add(this.chkInherit);
-            this.Controls.Add(this.bttnCancel);
-            this.Controls.Add(this.bttnUpdate);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Name = "IdentityColumnsThresholdConfig";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Identity Columns Threshold Configuration";
-            this.Load += new System.EventHandler(this.IdentityColumnsThresholdConfig_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.numWarning)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numCritical)).EndInit();
-            this.pnlConfig.ResumeLayout(false);
-            this.pnlConfig.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(444, 195);
+            Controls.Add(bttnUp);
+            Controls.Add(pnlConfig);
+            Controls.Add(lblObject);
+            Controls.Add(chkInherit);
+            Controls.Add(bttnCancel);
+            Controls.Add(bttnUpdate);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            Name = "IdentityColumnsThresholdConfig";
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            Text = "Identity Columns Threshold Configuration";
+            Load += IdentityColumnsThresholdConfig_Load;
+            ((System.ComponentModel.ISupportInitialize)numWarning).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numCritical).EndInit();
+            pnlConfig.ResumeLayout(false);
+            pnlConfig.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
