c65e4005baf0dcfcc839f967f827fa8e7010931c
David Wiseman <33157668+DavidWiseman@users.noreply.github.com>
2024-10-30 14:31:07 +0000

    Configuration - New Grid
    Swap out the default DataGridView for the custom one with context menu options for filtering.
    Note: Filtering not supported for main grid as it's not bound to a DataView.

--- DBADashGUI/Changes/ConfigurationHistory.cs
+++ DBADashGUI/Changes/ConfigurationHistory.cs
@@ -12,6 +12,7 @@ namespace DBADashGUI
         public ConfigurationHistory()
         {
             InitializeComponent();
+            dgv.RegisterClearFilter(tsClearFilter);
         }
 
         private List<int> InstanceIDs;
@@ -26,22 +27,25 @@ namespace DBADashGUI
         {
             configuration1.InstanceIDs = InstanceIDs;
             configuration1.RefreshData();
-            using (var cn = new SqlConnection(Common.ConnectionString))
-            using (var cmd = new SqlCommand("dbo.SysConfigHistory_Get", cn) { CommandType = CommandType.StoredProcedure })
-            using (var da = new SqlDataAdapter(cmd))
-            {
-                cn.Open();
-                cmd.Parameters.AddWithValue("@InstanceIDs", string.Join(",", InstanceIDs));
-                cmd.Parameters.AddWithValue("ShowHidden", InstanceIDs.Count == 1 || Common.ShowHidden);
-
-                DataTable dt = new();
-                da.Fill(dt);
-                DateHelper.ConvertUTCToAppTimeZone(ref dt);
-                dgv.AutoGenerateColumns = false;
-                dgv.DataSource = dt;
-                dgv.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.DisplayedCells);
-            }
+            dgv.AutoGenerateColumns = false;
+            dgv.DataSource = new DataView(GetConfigHistory());
+            dgv.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.DisplayedCells);
             dgv.Columns[0].Frozen = Common.FreezeKeyColumn;
+        }
+
+        private DataTable GetConfigHistory()
+        {
+            using var cn = new SqlConnection(Common.ConnectionString);
+            using var cmd = new SqlCommand("dbo.SysConfigHistory_Get", cn) { CommandType = CommandType.StoredProcedure };
+            using var da = new SqlDataAdapter(cmd);
+            cn.Open();
+            cmd.Parameters.AddWithValue("@InstanceIDs", string.Join(",", InstanceIDs));
+            cmd.Parameters.AddWithValue("ShowHidden", InstanceIDs.Count == 1 || Common.ShowHidden);
+
+            DataTable dt = new();
+            da.Fill(dt);
+            DateHelper.ConvertUTCToAppTimeZone(ref dt);
+            return dt;
         }
 
         private void TsRefresh_Click(object sender, EventArgs e)
@@ -51,12 +55,12 @@ namespace DBADashGUI
 
         private void TsCopy_Click(object sender, EventArgs e)
         {
-            Common.CopyDataGridViewToClipboard(dgv);
+            dgv.CopyGrid();
         }
 
         private void TsHistoryExcel_Click(object sender, EventArgs e)
         {
-            Common.PromptSaveDataGridView(ref dgv);
+            dgv.ExportToExcel();
         }
     }
 }
