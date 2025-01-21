6278523b8fc20548001b2f97c36cbb340acb5d00
David Wiseman <wiseman1982@googlemail.com>
2025-01-21 18:44:07 +0000

    Fix issue deleting a rule with active alerts
    Close any existing alerts that reference a rule before deleting it.
    #1186

--- DBADashDB/Alert/Procedures/Rule_Del.sql
+++ DBADashDB/Alert/Procedures/Rule_Del.sql
@@ -3,6 +3,23 @@
 )
 AS
 SET NOCOUNT ON 
+SET XACT_ABORT ON
+
+DECLARE @AlertIDs BigIDs
+
+BEGIN TRAN
+
+INSERT INTO @AlertIDs(ID)
+SELECT AlertID 
+FROM Alert.ActiveAlerts
+WHERE RuleID = @RuleID
+
+IF @@ROWCOUNT>0 /* Close any existing alerts that reference the rule */
+BEGIN
+	EXEC Alert.ClosedAlerts_Add @AlertIDs=@AlertIDs
+END
 
 DELETE Alert.Rules 
 WHERE RuleID = @RuleID
+
+COMMIT
