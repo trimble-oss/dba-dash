e3172178da144f20ec8d9a78f667e7f048c01177
Chad Hodgson <82594173+j005e@users.noreply.github.com>
2025-02-11 08:30:11 -0600

    Update SQLDrives.sql
    Using UPPER in the SELECT and GROUP BY statements mitigates an issue related to Drive Letter case sensitivity in database file physical paths (e.g., D:\filepath and d:\filepath treated as separate volumes).

--- DBADash/SQL/SQLDrives.sql
+++ DBADash/SQL/SQLDrives.sql
@@ -4,23 +4,23 @@ BEGIN
 END
 ELSE IF  SERVERPROPERTY('EngineEdition') = 8
 BEGIN
-	SELECT  dovs.volume_mount_point AS Name,
+	SELECT  UPPER(dovs.volume_mount_point) AS Name,
 			AVG(dovs.total_bytes + dovs.available_bytes) as Capacity, /* Total Bytes = Used for Azure MI */
 			AVG(dovs.available_bytes) as FreeSpace,
 			dovs.logical_volume_name as Label
 	FROM sys.master_files mf
 	CROSS APPLY sys.dm_os_volume_stats(mf.database_id, mf.file_id) dovs
-	GROUP BY dovs.volume_mount_point,
+	GROUP BY UPPER(dovs.volume_mount_point),
 			 dovs.logical_volume_name;
 END
 ELSE
 BEGIN
-	SELECT  dovs.volume_mount_point AS Name,
+	SELECT  UPPER(dovs.volume_mount_point) AS Name,
 			AVG(dovs.total_bytes) as Capacity,
 			AVG(dovs.available_bytes) as FreeSpace,
 			dovs.logical_volume_name as Label
 	FROM sys.master_files mf
 	CROSS APPLY sys.dm_os_volume_stats(mf.database_id, mf.file_id) dovs
-	GROUP BY dovs.volume_mount_point,
+	GROUP BY UPPER(dovs.volume_mount_point),
 			 dovs.logical_volume_name;
 END
