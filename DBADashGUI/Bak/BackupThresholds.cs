44d2f33c52e2c0e72e063a439c756a072eec3f97
David Wiseman <wiseman1982@googlemail.com>
2025-03-23 15:13:13 +0000

    Refactoring

--- DBADashGUI/Bak/BackupThresholds.cs
+++ DBADashGUI/Bak/BackupThresholds.cs
@@ -32,72 +32,69 @@ namespace DBADashGUI.Backups
         public static BackupThresholds GetThresholds(int InstanceID, int DatabaseID)
         {
             BackupThresholds thresholds = new();
-            using (var cn = new SqlConnection(Common.ConnectionString))
-            using (var cmd = new SqlCommand("dbo.BackupThresholds_Get", cn) { CommandType = CommandType.StoredProcedure })
+            using var cn = new SqlConnection(Common.ConnectionString);
+            using var cmd = new SqlCommand("dbo.BackupThresholds_Get", cn) { CommandType = CommandType.StoredProcedure };
+            cn.Open();
+            cmd.Parameters.AddWithValue("InstanceID", InstanceID);
+            cmd.Parameters.AddWithValue("DatabaseID", DatabaseID);
+            using var rdr = cmd.ExecuteReader();
+            if (rdr.Read())
             {
-                cn.Open();
-                cmd.Parameters.AddWithValue("InstanceID", InstanceID);
-                cmd.Parameters.AddWithValue("DatabaseID", DatabaseID);
-                var rdr = cmd.ExecuteReader();
-                if (rdr.Read())
+                if (rdr["FullBackupCriticalThreshold"] != DBNull.Value && rdr["FullBackupWarningThreshold"] != DBNull.Value)
                 {
-                    if (rdr["FullBackupCriticalThreshold"] != DBNull.Value && rdr["FullBackupWarningThreshold"] != DBNull.Value)
-                    {
-                        thresholds.FullCritical = (int)rdr["FullBackupCriticalThreshold"];
-                        thresholds.FullWarning = (int)rdr["FullBackupWarningThreshold"];
-                    }
-                    if (rdr["DiffBackupCriticalThreshold"] != DBNull.Value && rdr["DiffBackupWarningThreshold"] != DBNull.Value)
-                    {
-                        thresholds.DiffCritical = (int)rdr["DiffBackupCriticalThreshold"];
-                        thresholds.DiffWarning = (int)rdr["DiffBackupWarningThreshold"];
-                    }
+                    thresholds.FullCritical = (int)rdr["FullBackupCriticalThreshold"];
+                    thresholds.FullWarning = (int)rdr["FullBackupWarningThreshold"];
+                }
+                if (rdr["DiffBackupCriticalThreshold"] != DBNull.Value && rdr["DiffBackupWarningThreshold"] != DBNull.Value)
+                {
+                    thresholds.DiffCritical = (int)rdr["DiffBackupCriticalThreshold"];
+                    thresholds.DiffWarning = (int)rdr["DiffBackupWarningThreshold"];
+                }
 
-                    if (rdr["LogBackupCriticalThreshold"] != DBNull.Value && rdr["LogBackupWarningThreshold"] != DBNull.Value)
-                    {
-                        thresholds.LogCritical = (int)rdr["LogBackupCriticalThreshold"];
-                        thresholds.LogWarning = (int)rdr["LogBackupWarningThreshold"];
-                    }
-                    thresholds.MinimumAge = rdr["MinimumAge"] == DBNull.Value ? 0 : (int)rdr["MinimumAge"];
-                    thresholds.UsePartial = (bool)rdr["ConsiderPartialBackups"];
-                    thresholds.UseFG = (bool)rdr["ConsiderFGBackups"];
-                    thresholds.ExcludedDBs = Convert.ToString(rdr["ExcludedDatabases"]);
-                    thresholds.ConsiderCopyOnlyBackups = (bool)rdr["ConsiderCopyOnlyBackups"];
-                    thresholds.ConsiderSnapshotBackups = (bool)rdr["ConsiderSnapshotBackups"];
-                    thresholds.ConsiderFullBackupWithDiffThreshold = (bool)rdr["ConsiderFullBackupWithDiffThreshold"];
-                }
-                else
+                if (rdr["LogBackupCriticalThreshold"] != DBNull.Value && rdr["LogBackupWarningThreshold"] != DBNull.Value)
                 {
-                    thresholds.Inherit = true;
+                    thresholds.LogCritical = (int)rdr["LogBackupCriticalThreshold"];
+                    thresholds.LogWarning = (int)rdr["LogBackupWarningThreshold"];
                 }
+                thresholds.MinimumAge = rdr["MinimumAge"] == DBNull.Value ? 0 : (int)rdr["MinimumAge"];
+                thresholds.UsePartial = (bool)rdr["ConsiderPartialBackups"];
+                thresholds.UseFG = (bool)rdr["ConsiderFGBackups"];
+                thresholds.ExcludedDBs = Convert.ToString(rdr["ExcludedDatabases"]);
+                thresholds.ConsiderCopyOnlyBackups = (bool)rdr["ConsiderCopyOnlyBackups"];
+                thresholds.ConsiderSnapshotBackups = (bool)rdr["ConsiderSnapshotBackups"];
+                thresholds.ConsiderFullBackupWithDiffThreshold = (bool)rdr["ConsiderFullBackupWithDiffThreshold"];
             }
+            else
+            {
+                thresholds.Inherit = true;
+            }
+
             return thresholds;
         }
 
         public void Save()
         {
-            using (var cn = new SqlConnection(Common.ConnectionString))
-            using (var cmd = new SqlCommand("dbo.BackupThresholds_Upd", cn) { CommandType = CommandType.StoredProcedure })
-            {
-                cn.Open();
-                cmd.Parameters.AddWithValue("InstanceID", InstanceID);
-                cmd.Parameters.AddWithValue("DatabaseID", DatabaseID);
-                if (FullWarning != null) { cmd.Parameters.AddWithValue("FullWarning", FullWarning); }
-                if (FullCritical != null) { cmd.Parameters.AddWithValue("FullCritical", FullCritical); }
-                if (DiffWarning != null) { cmd.Parameters.AddWithValue("DiffWarning", DiffWarning); }
-                if (DiffCritical != null) { cmd.Parameters.AddWithValue("DiffCritical", DiffCritical); }
-                if (LogWarning != null) { cmd.Parameters.AddWithValue("LogWarning", LogWarning); }
-                if (LogCritical != null) { cmd.Parameters.AddWithValue("LogCritical", LogCritical); }
-                cmd.Parameters.AddWithValue("UseFG", UseFG);
-                cmd.Parameters.AddWithValue("UsePartial", UsePartial);
-                cmd.Parameters.AddWithValue("Inherit", Inherit);
-                cmd.Parameters.AddWithValue("ExcludedDatabases", ExcludedDBs);
-                if (MinimumAge > 0) { cmd.Parameters.AddWithValue("MinimumAge", MinimumAge); }
+            using var cn = new SqlConnection(Common.ConnectionString);
+            using var cmd = new SqlCommand("dbo.BackupThresholds_Upd", cn) { CommandType = CommandType.StoredProcedure };
+            cn.Open();
+            cmd.Parameters.AddWithValue("InstanceID", InstanceID);
+            cmd.Parameters.AddWithValue("DatabaseID", DatabaseID);
+            if (FullWarning != null) { cmd.Parameters.AddWithValue("FullWarning", FullWarning); }
+            if (FullCritical != null) { cmd.Parameters.AddWithValue("FullCritical", FullCritical); }
+            if (DiffWarning != null) { cmd.Parameters.AddWithValue("DiffWarning", DiffWarning); }
+            if (DiffCritical != null) { cmd.Parameters.AddWithValue("DiffCritical", DiffCritical); }
+            if (LogWarning != null) { cmd.Parameters.AddWithValue("LogWarning", LogWarning); }
+            if (LogCritical != null) { cmd.Parameters.AddWithValue("LogCritical", LogCritical); }
+            cmd.Parameters.AddWithValue("UseFG", UseFG);
+            cmd.Parameters.AddWithValue("UsePartial", UsePartial);
+            cmd.Parameters.AddWithValue("Inherit", Inherit);
+            cmd.Parameters.AddWithValue("ExcludedDatabases", ExcludedDBs);
+            if (MinimumAge > 0) { cmd.Parameters.AddWithValue("MinimumAge", MinimumAge); }
 
-                cmd.Parameters.AddWithValue("ConsiderCopyOnlyBackups", ConsiderCopyOnlyBackups);
-                cmd.Parameters.AddWithValue("ConsiderSnapshotBackups", ConsiderSnapshotBackups);
-                cmd.Parameters.AddWithValue("ConsiderFullBackupWithDiffThreshold", ConsiderFullBackupWithDiffThreshold);
-                cmd.ExecuteNonQuery();
-            }
+            cmd.Parameters.AddWithValue("ConsiderCopyOnlyBackups", ConsiderCopyOnlyBackups);
+            cmd.Parameters.AddWithValue("ConsiderSnapshotBackups", ConsiderSnapshotBackups);
+            cmd.Parameters.AddWithValue("ConsiderFullBackupWithDiffThreshold", ConsiderFullBackupWithDiffThreshold);
+            cmd.ExecuteNonQuery();
         }
     }
 }
