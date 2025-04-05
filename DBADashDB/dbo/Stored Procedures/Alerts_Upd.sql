16118f661bf47afaf10b40ee93e04d767809c2d2
David Wiseman <wiseman1982@googlemail.com>
2025-04-05 17:47:34 +0100

    Add SQL Agent Alert Rule
    Add an alert rule for SQL Agent alerts

--- DBADashDB/dbo/Stored Procedures/Alerts_Upd.sql
+++ DBADashDB/dbo/Stored Procedures/Alerts_Upd.sql
@@ -13,7 +13,91 @@ IF NOT EXISTS(	SELECT 1
 				AND Reference=@Ref
 			)
 BEGIN
+	DECLARE @Type VARCHAR(50)='SQLAgentAlert'
+	DECLARE @AlertDetails Alert.AlertDetails
+	DECLARE @AlertDetailCount INT=0
+
+	/* Check if we have any DBA Dash alert rules that might apply */
+	IF EXISTS(	SELECT 1 
+				FROM Alert.Rules 
+				WHERE Type = @Type 
+				AND IsActive=1
+				)
+	BEGIN
+		CREATE TABLE #NewAlerts(
+			name NVARCHAR(MAX),
+			message_id INT,
+			severity INT,
+			notification_message NVARCHAR(512)
+		)
+		/* Get new alerts - alerts that have been triggered since the last collection */
+		INSERT INTO #NewAlerts(
+			name,
+			message_id,
+			severity,
+			notification_message
+		)
+		SELECT	T.name,
+				T.message_id,
+				T.severity,
+				T.notification_message
+		FROM @Alerts T
+		WHERE NOT EXISTS(
+						SELECT 1 
+						FROM dbo.Alerts A
+						WHERE A.InstanceID = @InstanceID
+						AND A.last_occurrence >=T.last_occurrence 
+						AND A.id = t.id
+						)
+		AND T.last_occurrence >=DATEADD(d,-1,GETUTCDATE()) /* Put a limit on last occurance date (for new instances) */
+
+		IF @@ROWCOUNT >0
+		BEGIN
+			/* Filter the new sql agent alerts based on the DBA Dash alert rules */
+			INSERT INTO @AlertDetails(
+					InstanceID,
+					Priority,
+					AlertKey,
+					Message,
+					RuleID
+			)
+			SELECT @InstanceID,
+					R.Priority,
+					REPLACE(R.AlertKey,'{AlertName}',name) AS AlertKey,
+					ISNULL(T.notification_message,name + ' alert triggered'),
+					R.RuleID
+			FROM Alert.Rules R
+			CROSS APPLY Alert.ApplicableInstances_Get(R.ApplyToTagID,R.ApplyToInstanceID,R.AlertKey,R.ApplyToHidden) I
+			OUTER APPLY(SELECT 	TRY_CAST(JSON_VALUE(R.Details,'$.SeverityFrom') AS INT) AS SeverityFrom,
+								TRY_CAST(JSON_VALUE(R.Details,'$.SeverityTo') AS INT) AS SeverityTo,
+								ISNULL(JSON_VALUE(R.Details,'$.AlertName'),'%') AS AlertName
+						) calc
+			CROSS APPLY #NewAlerts T
+			WHERE R.Type = @Type
+			AND R.IsActive=1
+			AND I.InstanceID = @InstanceID
+			AND (T.severity>=calc.SeverityFrom OR calc.SeverityFrom IS NULL)
+			AND (T.severity<=calc.SeverityTo OR calc.SeverityTo IS NULL)
+			AND (T.name LIKE calc.AlertName)
+			AND (	
+					EXISTS(	
+							SELECT 1
+							FROM OPENJSON(R.Details,'$.MessageIDList') List 
+							WHERE List.Value = T.message_id
+						)
+					OR JSON_QUERY(R.Details,'$.MessageIDList') ='[]'
+				)
+			SET @AlertDetailCount = @@ROWCOUNT
+		END
+	END
+		
+
 	BEGIN TRAN
+
+	IF @AlertDetailCount >0 /* Update DBA Dash alerts if needed */
+	BEGIN
+		EXEC Alert.ActiveAlerts_Upd @AlertDetails=@AlertDetails,@AlertType=@Type,@ResolveAlertsOfType=0
+	END
 
 	DELETE A 
 	FROM dbo.Alerts A
---
+++ DBADashGUI/DBADashAlerts/Rules/SQLAgentAlertRule.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBADashGUI.DBADashAlerts.Rules
+{
+    internal class SQLAgentAlertRule : AlertRuleBase
+    {
+        public override string AlertKey => "Alert: {AlertName}";
+
+        public override RuleTypes RuleType => RuleTypes.SQLAgentAlert;
+
+        [System.Text.Json.Serialization.JsonIgnore]
+        [Browsable(false)]
+        public override decimal? Threshold => null;
+
+        [System.Text.Json.Serialization.JsonIgnore]
+        [Browsable(false)]
+        public override int? EvaluationPeriodMins => null;
+
+        [DisplayName("Message IDs"), Category("Filters"),
+         Description("Message IDs to filter on.  Leave empty to apply to all Message IDs")]
+        public List<int> MessageIDList { get; set; } = new();
+
+        [DisplayName("Severity From"), Category("Filters"), Description("Minimum Severity.  Leave blank to apply to all severities")]
+        public int? SeverityFrom { get; set; }
+
+        [DisplayName("Severity To"), Category("Filters"), Description("Maximum Severity.  Leave blank to apply to all severities")]
+        public int? SeverityTo { get; set; }
+
+        [DisplayName("Alert Name"), Category("Filters"), Description("Name of the SQL Agent alert to filter on.  Leave blank to apply to all.")]
+        public string AlertName { get; set; }
+
+        public override (bool isValid, string message) Validate()
+        {
+            return (true, string.Empty);
+        }
+    }
+}
