223100912aefd6c25fb070a0b35dded94d3ca365
David Wiseman <dave_wiseman@trimble.com>
2025-04-24 16:36:54 +0100

    Running Queries - Tabbed viewer
    Add context menu to load selected snapshots in a new Window.  If multiple snapshots are selected, each snapshot is loaded into a selected tab.

---
+++ DBADashGUI/Performance/RunningQueriesSnapshotInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBADashGUI.Performance
+{
+    public class RunningQueriesSnapshotInfo
+    {
+        public RunningQueriesSnapshotInfo(DateTime snapshotDateUtc, int instanceId, string title)
+        {
+            SnapshotDateUtc = snapshotDateUtc;
+            InstanceID = instanceId;
+            Title = title;
+        }
+
+        public DateTime SnapshotDateUtc { get; }
+        public int InstanceID { get; }
+
+        public string Title { get; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+            var other = (RunningQueriesSnapshotInfo)obj;
+            return (SnapshotDateUtc == other.SnapshotDateUtc) && (InstanceID == other.InstanceID);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(SnapshotDateUtc, InstanceID);
+        }
+    }
+}
--- DBADashGUI/Performance/RunningQueriesViewer.cs
+++ DBADashGUI/Performance/RunningQueriesViewer.cs
@@ -1,5 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Windows.Forms;
+using Amazon.S3.Model.Internal.MarshallTransformations;
+using DBADashGUI.Theme;
+using DocumentFormat.OpenXml.Office2010.ExcelAc;
 
 namespace DBADashGUI.Performance
 {
@@ -32,12 +39,88 @@ namespace DBADashGUI.Performance
             get => runningQueries1.JobId; set => runningQueries1.JobId = value;
         }
 
+        public ThemedTabControl Tab;
+
+        public void LoadSnapshots(List<RunningQueriesSnapshotInfo> snapshots)
+        {
+            if (snapshots.Count == 0) return;
+            Tab = new ThemedTabControl() { Dock = DockStyle.Fill };
+            Tab.SelectedIndexChanged += Tab_SelectedIndexChanged;
+            var i = 0;
+            foreach (var snapshot in snapshots)
+            {
+                var page = new TabPage(snapshot.Title);
+                Tab.TabPages.Add(page);
+                if (i == 0)
+                {
+                    page.Controls.Add(runningQueries1);
+                    runningQueries1.SnapshotDateFrom = snapshot.SnapshotDateUtc;
+                    runningQueries1.SnapshotDateTo = snapshot.SnapshotDateUtc;
+                    runningQueries1.InstanceID = snapshot.InstanceID;
+                }
+                else
+                {
+                    var viewerControl = new RunningQueries()
+                    {
+                        SnapshotDateFrom = snapshot.SnapshotDateUtc,
+                        SnapshotDateTo = snapshot.SnapshotDateUtc,
+                        InstanceID = snapshot.InstanceID,
+                        Dock = DockStyle.Fill,
+                    };
+                    page.Controls.Add(viewerControl);
+                }
+                i++;
+            }
+            Tab.ApplyTheme();
+            this.Controls.Add(Tab);
+        }
+
+        public HashSet<int> ResizedTabs = new();
+        public HashSet<int> LoadedTabs = new();
+
+        private void Tab_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (Tab.SelectedTab is null) return;
+            if (ResizedTabs.Contains(Tab.SelectedIndex)) return;
+            var runningQueries = Tab.SelectedTab.Controls.OfType<RunningQueries>().First();
+            if (!LoadedTabs.Contains(Tab.SelectedIndex))
+            {
+                runningQueries.RefreshData();
+                LoadedTabs.Add(Tab.SelectedIndex);
+            }
+            ResizedTabs.Add(Tab.SelectedIndex);
+            runningQueries.AutoResizeColumnsWithMaxColumnWidth();
+        }
+
         private void RunningQueriesViewer_Load(object sender, EventArgs e)
         {
-            runningQueries1.RefreshData();
-            if (ShowRootBlockers)
+            RefreshAll();
+        }
+
+        private void RefreshAll()
+        {
+            if (Tab != null)
             {
-                runningQueries1.ShowRootBlockers();
+                const int maxTabs = 5; // Load max of 5 snapshots upfront, then load on demand
+                for (var i = 0; i < maxTabs && i < Tab.TabPages.Count; i++)
+                {
+                    var viewerControl = Tab.TabPages[i].Controls.OfType<RunningQueries>().First();
+                    viewerControl.RefreshData();
+                    if (ShowRootBlockers)
+                    {
+                        viewerControl.ShowRootBlockers();
+                    }
+
+                    LoadedTabs.Add(i);
+                }
+            }
+            else
+            {
+                runningQueries1.RefreshData();
+                if (ShowRootBlockers)
+                {
+                    runningQueries1.ShowRootBlockers();
+                }
             }
         }
     }
