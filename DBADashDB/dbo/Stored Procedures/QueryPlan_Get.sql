6bc81e683491d83df206e91043cf01ed4160a2c5
Chad Baldwin <cbaldwin@hchb.com>
2025-04-29 23:50:07 -0700

    Remove query_plan from RunningQueries proc output, changed to lazy load query plan on demand

---
+++ DBADashDB/dbo/Stored Procedures/QueryPlan_Get.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE dbo.QueryPlan_Get (
+	@plan_handle varbinary(64),
+	@query_plan_hash binary(8),
+	@statement_start_offset int,
+	@statement_end_offset int
+)
+AS
+BEGIN;
+	SELECT QP.query_plan_compresed
+	FROM dbo.QueryPlans QP
+	WHERE QP.plan_handle = @plan_handle
+		AND QP.query_plan_hash = @query_plan_hash
+		AND QP.statement_start_offset = @statement_start_offset
+		AND QP.statement_end_offset = @statement_end_offset
+END;
