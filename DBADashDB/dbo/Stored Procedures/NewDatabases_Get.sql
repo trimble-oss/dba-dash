57e1a7344488adca6807a9204cf9028c494fc2f3
David Wiseman <33157668+DavidWiseman@users.noreply.github.com>
2025-09-11 14:58:08 +0100

    Add New Databases system report
    Add new databases system report to list recently created databases across instances.
    Update Table Size and Deleted Databases report to show pickers on the menu.

---
+++ DBADashDB/dbo/Stored Procedures/NewDatabases_Get.sql
@@ -0,0 +1,33 @@
+﻿CREATE  PROC dbo.NewDatabases_Get(
+	@InstanceIDs IDs READONLY,
+	@Days INT=7
+)
+AS
+SELECT	I.InstanceID,
+		I.InstanceGroupName,
+		D.name AS DB,
+		DATEADD(mi,I.UTCOffset,D.create_date) AS CreatedDate,
+		(SELECT TOP(1) SUM(SS.Size)/128.0
+		FROM dbo.DBFiles F 
+		/* Get latest snapshot for each file */
+		OUTER APPLY(SELECT TOP(1)	SS.Size,
+									SS.SnapshotDate
+					FROM dbo.DBFileSnapshot SS
+					WHERE SS.FileID = F.FileID
+					ORDER BY SS.SnapshotDate DESC
+					)	SS		
+		WHERE F.DatabaseID = D.DatabaseID
+		GROUP BY SS.SnapshotDate /* Group and SUM by SnapshotDate */
+		ORDER BY SS.SnapshotDate DESC /* Only return sum of files included in the last snapshot */
+		) SizeMB
+FROM dbo.Databases D 
+JOIN dbo.Instances I ON D.InstanceID = I.InstanceID
+WHERE D.IsActive = 1
+AND EXISTS(SELECT 1
+			FROM @InstanceIDs T
+			WHERE T.ID = I.InstanceID
+			)
+AND DATEADD(mi,I.UTCOffset,D.create_date) >= DATEADD(d,-@Days,SYSUTCDATETIME())
+AND D.database_id > 4 /* Exclude system DBs */
+ORDER BY D.create_date DESC
+
--- DBADashGUI/CustomReports/SystemReports.cs
+++ DBADashGUI/CustomReports/SystemReports.cs
@@ -8,6 +8,7 @@ namespace DBADashGUI.CustomReports
         {
             Add(DatabaseFinder);
             Add(DeletedDatabases);
+            Add(NewDatabases);
             Add(ServerRoleMembers);
             Add(ServerServices);
             Add(TableSizeHistory);
@@ -55,7 +56,8 @@ namespace DBADashGUI.CustomReports
                         { "SQL Server", "SQL Server" },
                         { "SQL Server Agent", "SQL Server Agent" },
                         { "", "ALL" }
-                    }
+                    },
+                    MenuBar = true
                 },
             },
             TriggerCollectionTypes = { DBADash.CollectionType.ServerServices.ToString() },
@@ -335,8 +337,10 @@ namespace DBADashGUI.CustomReports
                             { "60", "30 Days" },
                             { "90", "90 Days" },
                             { "365", "365 Days" }
-                        }
+                        },
+                        MenuBar = true
                     },
+                    Picker.CreateTopPicker(true)
                 }
             };
 
@@ -613,8 +617,8 @@ namespace DBADashGUI.CustomReports
             },
             Pickers = new List<Picker>
             {
-                Picker.CreateTopPicker(),
-                Picker.CreateBooleanPicker("@ExcludeSystemDatabases","Exclude System Databases"),
+                Picker.CreateTopPicker(true),
+                Picker.CreateBooleanPicker("@ExcludeSystemDatabases","System Databases",true,"Exclude","Include",true),
                 new()
                 {
                     DataType = typeof(string),
@@ -704,17 +708,24 @@ namespace DBADashGUI.CustomReports
                 {
                     ParameterName = "@Days",
                     Name = "Days",
-                    DefaultValue = 7,
-                    PickerItems = new()
+                    PickerItems = new Dictionary<object, string>
                     {
+                        { 1, "1" },
+                        { 2, "2" },
+                        { 3, "3" },
+                        { 4, "4" },
+                        { 5, "5" },
+                        { 6, "6" },
                         { 7, "7" },
                         { 14, "14" },
                         { 30, "30" },
                         { 60, "60" },
                         { 90, "90" },
                         { 180, "180" },
-                        { 365, "365" }
+                        { 365, "365" },
                     },
+                    DefaultValue = 7,
+                    MenuBar = true,
                     DataType = typeof(int)
                 }
             },
@@ -740,6 +751,90 @@ namespace DBADashGUI.CustomReports
                 }
             }
         };
-
+        public static SystemReport NewDatabases => new()
+        {
+            SchemaName = "dbo",
+            ReportName = "New Databases",
+            Description = "Databases with a recent create_date (sys.databases)",
+            TriggerCollectionTypes = new List<string> { "Databases" },
+            ProcedureName = "NewDatabases_Get",
+            QualifiedProcedureName = "dbo.NewDatabases_Get",
+            Params = new Params
+            {
+                ParamList = new List<Param>
+                {
+                    new()
+                    {
+                        ParamName = "@Days",
+                        ParamType = "INT"
+                    },
+                    new()
+                    {
+                        ParamName = "@InstanceIDs",
+                        ParamType = "IDS"
+                    },
+                },
+            },
+            Pickers = new List<Picker>
+            {
+                new()
+                {
+                    ParameterName = "@Days",
+                    Name = "Days",
+                    PickerItems = new Dictionary<object, string>
+                    {
+                        { 1, "1" },
+                        { 2, "2" },
+                        { 3, "3" },
+                        { 4, "4" },
+                        { 5, "5" },
+                        { 6, "6" },
+                        { 7, "7" },
+                        { 14, "14" },
+                        { 30, "30" },
+                        { 60, "60" },
+                        { 90, "90" },
+                        { 180, "180" },
+                        { 365, "365" },
+                    },
+                    DefaultValue = 7,
+                    MenuBar = true,
+                    DataType = typeof(int)
+                }
+            },
+            CustomReportResults = new Dictionary<int, CustomReportResult>
+            {
+                {
+                    0, new CustomReportResult
+                    {
+                        ColumnAlias = new Dictionary<string, string>
+                        {
+                            { "InstanceGroupName", "Instance" },
+                            { "CreatedDate", "Created Date" },
+                            { "SizeMB", "Size (MB)" }
+                        },
+                        CellFormatString = new Dictionary<string, string>
+                        {
+                            { "SizeMB", "N0" }
+                        },
+                        ColumnLayout = new List<KeyValuePair<string, PersistedColumnLayout>>
+                        {
+                            new("InstanceID", new PersistedColumnLayout()),
+                            new("InstanceGroupName", new PersistedColumnLayout { Visible = true, DisplayIndex = 1 }),
+                            new("DB", new PersistedColumnLayout { Visible = true, DisplayIndex = 2 }),
+                            new("CreatedDate", new PersistedColumnLayout {  Visible = true, DisplayIndex = 3 }),
+                            new("SizeMB", new PersistedColumnLayout { Visible = true, DisplayIndex = 4 })
+                        },
+                        ResultName = "New Databases",
+                        LinkColumns = new Dictionary<string, LinkColumnInfo>
+                        {
+                            { "DB", new NavigateTreeLinkColumnInfo { InstanceColumn = "InstanceID", DatabaseColumn = "DB" } },
+                            { "SizeMB", new NavigateTreeLinkColumnInfo { InstanceColumn = "InstanceID", DatabaseColumn = "DB", Tab = Main.Tabs.DBSpace } },
+                            { "InstanceGroupName", new NavigateTreeLinkColumnInfo { InstanceColumn = "InstanceID", DatabaseColumn = "" } }
+                        },
+                    }
+                }
+            }
+        };
     }
 }
