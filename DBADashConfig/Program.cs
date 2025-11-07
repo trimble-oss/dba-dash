dda5ec80b25e718f513948ecca9495b16b070d81
David Wiseman <dave_wiseman@trimble.com>
2025-11-07 19:56:29 +0000

    DBADashConfig async fix
    Parser.Default.ParseArguments<Options>(args).WithParsed(async o => ...). An async lambda passed to WithParsed (which expects Action<Options>) becomes async void. Exceptions inside async void bypass normal Task exception propagation and can terminate the process.

--- DBADashConfig/Program.cs
+++ DBADashConfig/Program.cs
@@ -5,131 +5,141 @@ using Serilog;
 using System.Runtime.InteropServices;
 using static DBADashConfig.Options;
 
-Log.Logger = new LoggerConfiguration()
-   .WriteTo.Console(outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj} {NewLine}{Exception}")
-   .CreateLogger();
-
-Parser.Default.ParseArguments<Options>(args)
-      .WithParsed(async o =>
-      {
-          try
-          {
-              await ProcessParsed(o);
-          }
-          catch (Exception ex)
-          {
-              Log.Error(ex, ex.Message);
-              Environment.Exit(1);
-          }
-      });
-
-static async Task ProcessParsed(Options o)
+public class Program
 {
-    CollectionConfig config;
-    // Read config from json file or create a new config if the file doesn't exist
-    if (!(BasicConfig.ConfigExists))
+    public static async Task<int> Main(string[] args)
     {
-        Log.Information("Config file does not exist.  Starting with blank config");
-        config = new CollectionConfig();
+        Log.Logger = new LoggerConfiguration()
+           .WriteTo.Console(outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj} {NewLine}{Exception}")
+           .CreateLogger();
+
+        return await Parser.Default
+            .ParseArguments<Options>(args)
+            .MapResult(
+                async o =>
+                {
+                    try
+                    {
+                        await ProcessParsed(o);
+                        return 0;
+                    }
+                    catch (Exception ex)
+                    {
+                        Log.Error(ex, "Unhandled error");
+                        return 1;
+                    }
+                    finally
+                    {
+                        Log.CloseAndFlush();
+                    }
+                },
+                _ => Task.FromResult(1));
     }
-    else
+
+    private static async Task ProcessParsed(Options o)
     {
-        config = BasicConfig.Load<CollectionConfig>(o.DecryptionPassword);
-        if (o.SavePassword && !string.IsNullOrEmpty(o.DecryptionPassword) && RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        CollectionConfig config;
+        if (!BasicConfig.ConfigExists)
         {
-            Log.Information("Saving password");
-            EncryptedConfig.SavePassword(o.DecryptionPassword);
+            Log.Information("Config file does not exist. Starting with blank config");
+            config = new CollectionConfig();
         }
-    }
+        else
+        {
+            config = BasicConfig.Load<CollectionConfig>(o.DecryptionPassword);
+            if (o.SavePassword && !string.IsNullOrEmpty(o.DecryptionPassword) && RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Log.Information("Saving password");
+                EncryptedConfig.SavePassword(o.DecryptionPassword);
+            }
+        }
 
-    Log.Information("Action:" + o.Option);
-    switch (o.Option)
-    {
-        // Just return the name of the service
-        case CommandLineActionOption.GetServiceName:
-            Console.WriteLine(config.ServiceName);
-            Environment.Exit(0);
-            break;
+        Log.Information("Action: {Action}", o.Option);
 
-        case CommandLineActionOption.GetDestination:
-            Console.WriteLine(config.DestinationConnection.EncryptedConnectionString);
-            Environment.Exit(0);
-            break;
-        // List source and destination connections
-        case CommandLineActionOption.List:
-            Helper.ListConnections(config);
-            break;
+        switch (o.Option)
+        {
+            case CommandLineActionOption.GetServiceName:
+                Console.WriteLine(config.ServiceName);
+                break;
 
-        case CommandLineActionOption.List2:
-            Helper.ListConnections2(config);
-            break;
-        // Count connections
-        case CommandLineActionOption.Count:
-            Console.WriteLine(config.SourceConnections.Count);
-            Environment.Exit(0);
-            break;
-        // Add a new source connection
-        case CommandLineActionOption.Add:
-            await Helper.AddSourceConnectionAsync(config, o);
-            break;
+            case CommandLineActionOption.GetDestination:
+                Console.WriteLine(config.DestinationConnection.EncryptedConnectionString);
+                break;
 
-        case CommandLineActionOption.Remove:
-            await Helper.RemoveSourceConnectionAsync(o, config, false);
-            break;
+            case CommandLineActionOption.List:
+                Helper.ListConnections(config);
+                break;
 
-        case CommandLineActionOption.RemoveAndDelete:
-            await Helper.RemoveSourceConnectionAsync(o, config, true);
-            break;
+            case CommandLineActionOption.List2:
+                Helper.ListConnections2(config);
+                break;
 
-        case CommandLineActionOption.SetDestination:
-        case CommandLineActionOption.AddDestination:
-            Helper.AddDestination(config, o);
-            break;
+            case CommandLineActionOption.Count:
+                Console.WriteLine(config.SourceConnections.Count);
+                break;
 
-        case CommandLineActionOption.RemoveDestination:
-            Helper.RemoveDestination(config, o);
-            break;
+            case CommandLineActionOption.Add:
+                await Helper.AddSourceConnectionAsync(config, o);
+                break;
 
-        case CommandLineActionOption.CheckForUpdates:
-            Helper.CheckForUpdates();
-            break;
+            case CommandLineActionOption.Remove:
+                await Helper.RemoveSourceConnectionAsync(o, config, false);
+                break;
 
-        case CommandLineActionOption.Update:
-            Helper.CommandLineUpgrade();
-            break;
+            case CommandLineActionOption.RemoveAndDelete:
+                await Helper.RemoveSourceConnectionAsync(o, config, true);
+                break;
 
-        case CommandLineActionOption.SetServiceName:
-            Helper.SetServiceName(config, o);
-            break;
+            case CommandLineActionOption.SetDestination:
+            case CommandLineActionOption.AddDestination:
+                await Helper.AddDestination(config, o);
+                break;
 
-        case CommandLineActionOption.Encrypt:
-            Helper.EncryptConfig(config, o);
-            break;
+            case CommandLineActionOption.RemoveDestination:
+                Helper.RemoveDestination(config, o);
+                break;
 
-        case CommandLineActionOption.Decrypt:
-            config.EncryptionOption = BasicConfig.EncryptionOptions.Basic;
-            Helper.SaveConfig(config, o);
-            break;
+            case CommandLineActionOption.CheckForUpdates:
+                Helper.CheckForUpdates();
+                break;
 
-        case CommandLineActionOption.SetConfigFileBackupRetention:
-            config.ConfigBackupRetentionDays = o.RetentionDays;
-            Helper.SaveConfig(config, o);
-            break;
+            case CommandLineActionOption.Update:
+                Helper.CommandLineUpgrade();
+                break;
 
-        case CommandLineActionOption.Delete:
-            Helper.MarkInstanceDeleted(config, o.ConnectionID);
-            break;
+            case CommandLineActionOption.SetServiceName:
+                Helper.SetServiceName(config, o);
+                break;
 
-        case CommandLineActionOption.Restore:
-            Helper.MarkInstanceDeleted(config, o.ConnectionID, true);
-            break;
+            case CommandLineActionOption.Encrypt:
+                Helper.EncryptConfig(config, o);
+                break;
 
-        case CommandLineActionOption.PopulateConnectionID:
-            await Helper.PopulateConnectionIDAsync(config, o, false);
-            break;
+            case CommandLineActionOption.Decrypt:
+                config.EncryptionOption = BasicConfig.EncryptionOptions.Basic;
+                Helper.SaveConfig(config, o);
+                break;
 
-        case CommandLineActionOption.PopulateConnectionID2:
-            await Helper.PopulateConnectionIDAsync(config, o, true);
-            break;
+            case CommandLineActionOption.SetConfigFileBackupRetention:
+                config.ConfigBackupRetentionDays = o.RetentionDays;
+                Helper.SaveConfig(config, o);
+                break;
+
+            case CommandLineActionOption.Delete:
+                Helper.MarkInstanceDeleted(config, o.ConnectionID);
+                break;
+
+            case CommandLineActionOption.Restore:
+                Helper.MarkInstanceDeleted(config, o.ConnectionID, true);
+                break;
+
+            case CommandLineActionOption.PopulateConnectionID:
+                await Helper.PopulateConnectionIDAsync(config, o, false);
+                break;
+
+            case CommandLineActionOption.PopulateConnectionID2:
+                await Helper.PopulateConnectionIDAsync(config, o, true);
+                break;
+        }
     }
 }
