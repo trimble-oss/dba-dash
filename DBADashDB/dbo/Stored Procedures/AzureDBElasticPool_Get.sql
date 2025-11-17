a45176b351bef7ff9e88fa43e3b71a139225641b
David Wiseman <33157668+DavidWiseman@users.noreply.github.com>
2025-11-17 08:21:05 +0000

    Update how Elastic Pools are added to the tree
    Build using AzureDBElasticPool_Get rather than rely on there being an associated instance.  #1627

---
+++ DBADashDB/dbo/Stored Procedures/AzureDBElasticPool_Get.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROC dbo.AzureDBElasticPool_Get
+AS
+SELECT	EP.PoolID,
+		EP.elastic_pool_name,
+		I.InstanceGroupName
+FROM dbo.AzureDBElasticPool EP
+JOIN dbo.Instances I ON EP.InstanceID = I.InstanceID
