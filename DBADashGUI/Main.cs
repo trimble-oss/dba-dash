trimble-oss/dba-dash

commit fc3c698db7ded698ad34cef65a4b69c73ea8448b

Object Execution Tab - Add Query Store link
Add query store link to object execution tab, allowing you to quickly access query store data associated with a stored procedure.

DBADashGUI/Performance/ObjectExecutionSummary.cs
@@ -1,4 +1,5 @@
-﻿using DBADashGUI.Theme;
+﻿using DBADash;
+using DBADashGUI.Theme;
 using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
@@ -6,7 +7,6 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
-using DBADash;
 
 namespace DBADashGUI.Performance
 {
@@ -122,6 +122,7 @@ namespace DBADashGUI.Performance
                                                                         new DataGridViewTextBoxColumn()  { Name = "Total Writes", Visible=false,DataPropertyName = "total_writes", DefaultCellStyle = new DataGridViewCellStyle() { Format = "#,##0" } },
                                                                         new DataGridViewTextBoxColumn()  { Name = "Avg Writes",Visible=false, DataPropertyName = "avg_writes", DefaultCellStyle = new DataGridViewCellStyle() { Format = "#,##0" } },
                                                                         new DataGridViewTextBoxColumn()  { Name = "Period Time (sec)",Visible=false, DataPropertyName = "period_time_sec", DefaultCellStyle = new DataGridViewCellStyle() { Format = "#,##0.###" } },
+                                                                        new DataGridViewLinkColumn() { Name = "Query Store", Text = "Query Store", UseColumnTextForLinkValue = true }
         };
 
         private void TsColumn_Click(object sender, EventArgs e)
@@ -210,6 +211,7 @@ namespace DBADashGUI.Performance
                 tsCompare.Font = new Font(tsCompare.Font, HasCompare ? FontStyle.Bold : FontStyle.Regular);
                 dgv.DataSource = new DataView(dt, filter, "total_duration_sec DESC", DataViewRowState.CurrentRows);
                 dgv.Columns.AddRange(Columns.ToArray());
+                dgv.Columns["Query Store"].Visible = CurrentContext.CanMessage && CurrentContext.InstanceSupportsQueryStore;
                 dgv.ReplaceSpaceWithNewLineInHeaderTextToImproveColumnAutoSizing();
                 dgv.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.DisplayedCells);
                 dgv.ApplyTheme();
@@ -416,10 +418,20 @@ namespace DBADashGUI.Performance
         private void Dgv_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             if (e.RowIndex < 0) return;
+            var row = (DataRowView)dgv.Rows[e.RowIndex].DataBoundItem;
             if (dgv.Columns[e.ColumnIndex].Name == "Name")
             {
-                var row = (DataRowView)dgv.Rows[e.RowIndex].DataBoundItem;
                 RefreshChart((long)row["ObjectID"], (string)row["ObjectName"]);
+            }
+            else if (dgv.Columns[e.ColumnIndex].Name == "Query Store")
+            {
+                var qs = new QueryStoreViewer();
+                var context = CurrentContext.DeepCopy();
+                context.ObjectName = (string)row["ObjectName"];
+                context.ObjectID = (long)row["ObjectID"];
+                context.DatabaseName = (string)row["DB"];
+                qs.Context = context;
+                qs.Show();
             }
         }
 

DBADashGUI/Performance/QueryStoreTopQueries.cs
@@ -35,7 +35,7 @@ namespace DBADashGUI.Performance
             if (_context != CurrentContext)
             {
                 dgv.DataSource = null;
-                txtObjectName.Text = string.Empty;
+                txtObjectName.Text = _context.ObjectName;
                 txtPlan.Text = string.Empty;
                 splitContainer1.Panel2Collapsed = true;
                 dgvDrillDown.DataSource = null;
@@ -66,8 +66,8 @@ namespace DBADashGUI.Performance
 
         public async void RefreshData()
         {
-            txtObjectName.Enabled = QueryHash == null && PlanHash == null;
-            txtPlan.Enabled = QueryHash == null && PlanHash == null;
+            txtObjectName.Enabled = QueryHash == null && PlanHash == null && string.IsNullOrEmpty(CurrentContext.ObjectName);
+            txtPlan.Enabled = QueryHash == null && PlanHash == null && string.IsNullOrEmpty(CurrentContext.ObjectName);
             txtPlan.Text = PlanHash != null ? PlanHash.ToHexString(true) : txtPlan.Text;
             txtObjectName.Text = QueryHash != null ? QueryHash.ToHexString(true) : txtObjectName.Text;
 

DBADashGUI/DBADashContext.cs
@@ -57,7 +57,7 @@ namespace DBADashGUI
             get
             {
                 if (_hasInstanceMetadata != null) return _hasInstanceMetadata;
-                _hasInstanceMetadata = InstanceID >0 ? CommonData.HasInstanceMetadata.Contains(InstanceID) : InstanceIDs.Any(id => CommonData.HasInstanceMetadata.Contains(id));
+                _hasInstanceMetadata = InstanceID > 0 ? CommonData.HasInstanceMetadata.Contains(InstanceID) : InstanceIDs.Any(id => CommonData.HasInstanceMetadata.Contains(id));
                 return _hasInstanceMetadata;
             }
         }
@@ -133,9 +133,9 @@ namespace DBADashGUI
             }
 
             try
-            { 
+            {
                 var engineEditionValue = (int)row["EngineEdition"];
-            
+
                 if (Enum.IsDefined(typeof(DatabaseEngineEdition), engineEditionValue))
                 {
                     _engineEdition = (DatabaseEngineEdition)engineEditionValue;
@@ -152,6 +152,10 @@ namespace DBADashGUI
                 Log.Debug(ex, "Error retrieving EngineEdition value.");
             }
         }
+
+        public bool IsAzure => EngineEdition == DatabaseEngineEdition.SqlDatabase || EngineEdition == DatabaseEngineEdition.SqlManagedInstance;
+
+        public bool InstanceSupportsQueryStore => ProductVersion != null && (ProductVersion.Major >= 13 || IsAzure);
 
         public bool CanMessage
         {

DBADashGUI/Performance/QueryStoreViewer.cs
@@ -27,8 +27,12 @@ namespace DBADashGUI.Performance
         private void QueryStoreViewer_Load(object sender, EventArgs e)
         {
             this.ApplyTheme();
+            if (!string.IsNullOrEmpty(Context.ObjectName))
+            {
+                this.Text += " - " + Context.ObjectName;
+            }
             queryStoreTopQueries1.SetContext(Context);
-            if (PlanHash != null || QueryHash != null)
+            if (PlanHash != null || QueryHash != null || !string.IsNullOrEmpty(Context.ObjectName))
             {
                 queryStoreTopQueries1.RefreshData();
             }

commit 7e8a76ae7bb31683e30eebf8c91dad2db115920f

Query Store context bug fix
When setting context, the object name is assigned to the object name textbox when the tree type is instance or database.  This expects to filter on the name of a stored procedure - not the database or instance name.  Bug introduced in #1598.
Added a check on the tree type.  Updated object execution tab to pass the correct tree type.
Allow Query store tab to be visible at SP level.

DBADashGUI/Main.cs
@@ -1030,7 +1030,7 @@ namespace DBADashGUI
                     case SQLTreeItem.TreeType.StoredProcedure or SQLTreeItem.TreeType.CLRProcedure
                         or SQLTreeItem.TreeType.ScalarFunction or SQLTreeItem.TreeType.CLRScalarFunction
                         or SQLTreeItem.TreeType.Trigger or SQLTreeItem.TreeType.CLRTrigger:
-                        allowedTabs.AddRange(new[] { tabObjectExecutionSummary, tabPerformance, tabSchema });
+                        allowedTabs.AddRange(new[] { tabObjectExecutionSummary, tabPerformance, tabSchema, tabTopQueries });
 
                         foreach (var tool in CommunityTools.CommunityTools.ProcedureLevelTools)
                         {

DBADashGUI/Performance/ObjectExecutionSummary.cs
@@ -433,6 +433,7 @@ namespace DBADashGUI.Performance
                 context.ObjectName = (string)row["ObjectName"];
                 context.ObjectID = (long)row["ObjectID"];
                 context.DatabaseName = (string)row["DB"];
+                context.Type = SQLTreeItem.TreeType.StoredProcedure;
                 qs.Context = context;
                 qs.Show();
             }

DBADashGUI/Performance/QueryStoreTopQueries.cs
@@ -39,16 +39,29 @@ namespace DBADashGUI.Performance
         {
             if (_context != CurrentContext)
             {
-                dgv.DataSource = null;
-                txtObjectName.Text = _context.ObjectName;
-                txtPlan.Text = string.Empty;
-                splitContainer1.Panel2Collapsed = true;
-                dgvDrillDown.DataSource = null;
-                includeWaitsToolStripMenuItem.Enabled = _context.ProductVersion?.Major >= 14 || _context.AzureInstanceIDs.Count > 0;
-                lblStatus.Text = string.Empty;
+                CurrentContext = _context;
+                SetContext();
             }
-            tsExecute.Text = string.IsNullOrEmpty(_context.DatabaseName) ? "Execute (ALL Databases)" : "Execute";
-            CurrentContext = _context;
+        }
+
+        private void SetContext()
+        {
+            if (CurrentContext == null) return;
+            var isQueryStoreObject = CurrentContext.Type is SQLTreeItem.TreeType.StoredProcedure or SQLTreeItem.TreeType.CLRProcedure
+                        or SQLTreeItem.TreeType.ScalarFunction or SQLTreeItem.TreeType.CLRScalarFunction
+                        or SQLTreeItem.TreeType.Trigger or SQLTreeItem.TreeType.CLRTrigger;
+            var objectName = QueryHash != null ? QueryHash.ToHexString(true) : isQueryStoreObject ? CurrentContext.ObjectName : string.Empty;
+            var plan = PlanHash != null ? PlanHash.ToHexString(true) : string.Empty;
+            dgv.DataSource = null;
+            txtObjectName.Text = objectName;
+            txtObjectName.Enabled = objectName == string.Empty;
+            txtPlan.Text = plan;
+            txtPlan.Enabled = plan == string.Empty;
+            splitContainer1.Panel2Collapsed = true;
+            dgvDrillDown.DataSource = null;
+            includeWaitsToolStripMenuItem.Enabled = CurrentContext.ProductVersion?.Major >= 14 || CurrentContext.AzureInstanceIDs.Count > 0;
+            lblStatus.Text = string.Empty;
+            tsExecute.Text = string.IsNullOrEmpty(CurrentContext.DatabaseName) ? "Execute (ALL Databases)" : "Execute";
         }
 
         private int top = 25;
@@ -71,11 +84,6 @@ namespace DBADashGUI.Performance
 
         public async void RefreshData()
         {
-            txtObjectName.Enabled = QueryHash == null && PlanHash == null && string.IsNullOrEmpty(CurrentContext.ObjectName);
-            txtPlan.Enabled = QueryHash == null && PlanHash == null && string.IsNullOrEmpty(CurrentContext.ObjectName);
-            txtPlan.Text = PlanHash != null ? PlanHash.ToHexString(true) : txtPlan.Text;
-            txtObjectName.Text = QueryHash != null ? QueryHash.ToHexString(true) : txtObjectName.Text;
-
             if (lblStatus.Text == messageSentMessage)
             {
                 MessageBox.Show(@"Please wait for the current operation to complete", "Busy", MessageBoxButtons.OK,
@@ -744,8 +752,7 @@ namespace DBADashGUI.Performance
             SetGroupBy(QueryStoreTopQueriesMessage.QueryStoreGroupByEnum.query_id);
             SetSort("total_cpu_time_ms");
             SetTop(25);
-            txtPlan.Text = string.Empty;
-            txtObjectName.Text = string.Empty;
+            SetContext();
             tsNearestInterval.Checked = true;
             parallelPlansOnlyToolStripMenuItem.Checked = false;
             SetMinimumPlanCount(1);