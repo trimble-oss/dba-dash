882239810000fdc098c4868e7ddb66bbaab2c8a1
David Wiseman <dave_wiseman@trimble.com>
2025-12-13 17:22:50 +0000

    Tuning Recommendations
    Pull data directly from sys.dm_db_tuning_recommendations in a new tuning recommendations report.

--- DBADashGUI/CommunityTools/DirectExecutionReport.cs
+++ DBADashGUI/CommunityTools/DirectExecutionReport.cs
@@ -1,10 +1,14 @@
 ﻿namespace DBADashGUI.CommunityTools
 {
-    internal class DirectExecutionReport : CustomReports.CustomReport
+    public class DirectExecutionReport : CustomReports.CustomReport
     {
         public string DatabaseNameParameter { get; set; }
         public string ObjectParameterName { get; set; }
         public string SchemaParameterName { get; set; }
         public override bool IsDatabaseLevel => !string.IsNullOrEmpty(DatabaseNameParameter);
+
+        public override bool IsRootLevel => false;
+
+        public override bool IsInstanceLevel { get; } = true;
     }
 }
