8dd4692d5a49a172cb3de1c8443ad77322b8ccba
David Wiseman <33157668+DavidWiseman@users.noreply.github.com>
2026-01-12 08:11:55 +0000

    AvailabilityReplicas - update column
    Column should be NVARCHAR(256) instead of NVARCHAR(128) as documented.  Fixes potential string or binary data would be truncated error.
    #1707

--- DBADashDB/dbo/User Defined Types/AvailabilityReplicas.sql
+++ DBADashDB/dbo/User Defined Types/AvailabilityReplicas.sql
@@ -3,7 +3,7 @@
        group_id UNIQUEIDENTIFIER NOT NULL,
        replica_metadata_id INT NULL,
        replica_server_name NVARCHAR(256) NULL,
-       endpoint_url NVARCHAR(128) NULL,
+       endpoint_url NVARCHAR(256) NULL,
        availability_mode TINYINT NULL,
        failover_mode TINYINT NULL,
        session_timeout INT NULL,
