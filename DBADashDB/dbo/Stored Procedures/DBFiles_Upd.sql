69fe0d08a1857439f0b3e99cf2fc6b22b8c009a4
David Wiseman <33157668+DavidWiseman@users.noreply.github.com>
2026-01-24 12:32:56 +0000

    DBFiles_Upd - optimization
    Replace MERGE statement & optimize proc.  Update index on DBFiles table.

--- DBADashDB/dbo/Stored Procedures/DBFiles_Upd.sql
+++ DBADashDB/dbo/Stored Procedures/DBFiles_Upd.sql
@@ -1,23 +1,47 @@
-﻿
-
-CREATE PROC [dbo].[DBFiles_Upd](
+﻿CREATE PROC dbo.DBFiles_Upd(
 	@DBFiles DBFiles READONLY,
 	@InstanceID INT,
 	@SnapshotDate DATETIME2(2)
 )
 AS
+SET XACT_ABORT ON
+SET NOCOUNT ON
 DECLARE @Ref VARCHAR(30)='DBFiles'
 IF NOT EXISTS(SELECT 1 FROM dbo.CollectionDates WHERE SnapshotDate>=@SnapshotDate AND InstanceID = @InstanceID AND Reference=@Ref)
 BEGIN
-	WITH T AS (
-		SELECT F.* 
-		FROM dbo.Databases D
-		JOIN dbo.Instances I ON D.InstanceID = I.InstanceID
-		JOIN dbo.DBFiles F ON F.DatabaseID = D.DatabaseID
-		WHERE I.InstanceID = @InstanceID
+	CREATE TABLE #DBFiles(
+		DatabaseID INT NOT NULL,
+		file_id INT NOT NULL,
+		data_space_id INT NULL,
+		name sysname NOT NULL,
+		filegroup_name sysname NULL,
+		physical_name NVARCHAR(260) NULL,
+		type TINYINT NULL,
+		size BIGINT NULL,
+		space_used BIGINT NULL,
+		max_size BIGINT NULL,
+		growth BIGINT NULL,
+		is_percent_growth BIT NULL,
+		is_read_only BIT NULL,
+		state TINYINT NULL,
+		PRIMARY KEY (DatabaseID,file_id)
+	);
+	INSERT INTO #DBFiles(DatabaseID,
+		   file_id,
+		   data_space_id,
+		   name,
+		   filegroup_name,
+		   physical_name,
+		   type,
+		   size,
+		   space_used,
+		   max_size,
+		   growth,
+		   is_percent_growth,
+		   is_read_only,
+		   state
 	)
-	MERGE T
-	USING (SELECT D.DatabaseID
+	SELECT D.DatabaseID
 		  ,F.[file_id]
 		  ,F.[data_space_id]
 		  ,F.[name]
@@ -29,72 +53,98 @@ BEGIN
 		  ,F.[max_size]
 		  ,F.[growth]
 		  ,F.[is_percent_growth]
-		  ,F.[is_read_only] 
+		  ,F.[is_read_only]
 		  ,F.state
-		  FROM @DBFiles F 
-		  JOIN dbo.Databases D ON F.database_id = D.database_id
-		  WHERE D.IsActive=1
-		  AND D.InstanceID = @InstanceID) AS S ON S.file_id = T.file_id AND S.DatabaseID = T.DatabaseID
-	WHEN MATCHED THEN 
-	 UPDATE 
-	 SET T.[data_space_id]=S.[data_space_id]
-		  ,T.[name]=S.[name]
-		  ,T.[filegroup_name]=ISNULL(S.[filegroup_name],T.filegroup_name)
-		  ,T.[physical_name]=S.[physical_name]
-		  ,T.[type]=S.[type]
-		  ,T.[size]=S.[size]
-		  ,T.[space_used]=S.[space_used]
-		  ,T.[max_size]=S.[max_size]
-		  ,T.[growth]=S.[growth]
-		  ,T.[is_percent_growth]=S.[is_percent_growth]
-		  ,T.[is_read_only] =S.[is_read_only] 
-		  ,T.IsActive=1
-		  ,T.state = S.state
-	WHEN NOT MATCHED BY TARGET THEN
-	INSERT      (DatabaseID
-			   ,[file_id]
-			   ,[data_space_id]
-			   ,[name]
-			   ,[filegroup_name]
-			   ,[physical_name]
-			   ,[type]
-			   ,[size]
-			   ,[space_used]
-			   ,[max_size]
-			   ,[growth]
-			   ,[is_percent_growth]
-			   ,[is_read_only]
-			   ,IsActive
-			   ,state)
-	VALUES(DatabaseID
-		  ,[file_id]
-		  ,[data_space_id]
-		  ,[name]
-		  ,[filegroup_name]
-		  ,[physical_name]
-		  ,[type]
-		  ,[size]
-		  ,[space_used]
-		  ,[max_size]
-		  ,[growth]
-		  ,[is_percent_growth]
-		  ,[is_read_only]
-		  ,1
-		  ,state)
-	WHEN NOT MATCHED BY SOURCE THEN
-	UPDATE SET T.IsActive=0;
+	FROM @DBFiles F 
+	JOIN dbo.Databases D ON F.database_id = D.database_id
+	WHERE D.IsActive=1
+	AND D.InstanceID = @InstanceID
+	AND file_id IS NOT NULL
+
+
+	BEGIN TRAN
+
+	INSERT INTO dbo.DBFiles(
+		DatabaseID,
+		file_id,
+		data_space_id,
+		name,
+		filegroup_name,
+		physical_name,
+		type,
+		size,
+		space_used,
+		max_size,
+		growth,
+		is_percent_growth,
+		is_read_only,
+		IsActive,
+		state
+	)
+	SELECT T.DatabaseID,
+		   T.file_id,
+		   T.data_space_id,
+		   T.name,
+		   T.filegroup_name,
+		   T.physical_name,
+		   T.type,
+		   T.size,
+		   T.space_used,
+		   T.max_size,
+		   T.growth,
+		   T.is_percent_growth,
+		   T.is_read_only,
+		   CAST(1 AS BIT) AS IsActive,
+		   T.state
+	FROM #DBFiles T
+	WHERE NOT EXISTS
+	(
+		SELECT 1
+		FROM dbo.DBFiles F WITH (UPDLOCK)
+		WHERE F.file_id = T.file_id
+		AND F.DatabaseID = T.DatabaseID
+	);
+
+	UPDATE F
+		SET F.data_space_id = T.data_space_id,
+			F.name = T.name,
+			F.filegroup_name = ISNULL(T.filegroup_name, F.filegroup_name),
+			F.physical_name = T.physical_name,
+			F.type = T.type,
+			F.size = T.size,
+			F.space_used = T.space_used,
+			F.max_size = T.max_size,
+			F.growth = T.growth,
+			F.is_percent_growth = T.is_percent_growth,
+			F.is_read_only = T.is_read_only,
+			F.IsActive = 1,
+			F.state = T.state
+	FROM dbo.DBFiles F
+	JOIN #DBFiles T ON F.file_id = T.file_id
+						AND F.DatabaseID = T.DatabaseID;
+
+
+	UPDATE F
+	SET F.IsActive=0
+	FROM dbo.DBFiles F
+	JOIN dbo.Databases D ON F.DatabaseID = D.DatabaseID
+	WHERE NOT EXISTS(	SELECT 1
+						FROM #DBFiles T
+						WHERE T.DatabaseID = F.DatabaseID
+						AND T.file_id = F.file_id
+					)
+	AND D.InstanceID = @InstanceID
 
 	INSERT INTO dbo.DBFileSnapshot(SnapshotDate,FileID,Size,space_used)
 	SELECT @SnapshotDate,
-		  DBF.FileID,
-		  F.[size],
-		  F.[space_used]
-	FROM @DBFiles F 
-	JOIN dbo.Databases D ON F.database_id = D.database_id AND D.InstanceID = @InstanceID
-	JOIN dbo.DBFiles DBF ON F.file_id = DBF.file_id AND D.DatabaseID = DBF.DatabaseID
-	WHERE DBF.IsActive=1
-	AND D.IsActive=1
+		  F.FileID,
+		  T.size,
+		  T.space_used
+	FROM #DBFiles T 
+	JOIN dbo.DBFiles F ON T.file_id = F.file_id AND T.DatabaseID = F.DatabaseID
+	WHERE F.IsActive=1
 
+	COMMIT
 
 	EXEC dbo.CollectionDates_Upd @InstanceID = @InstanceID,  
 										 @Reference = @Ref,
