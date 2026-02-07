8175dbbd6a98ebca7102a32e20d1f978a7fb19e1
David Wiseman <dave_wiseman@trimble.com>
2026-02-07 08:26:25 +0000

    Blocking LiveCharts2
    Upgrade to LiveCharts2

--- DBADashGUI/Performance/Blocking.Designer.cs
+++ DBADashGUI/Performance/Blocking.Designer.cs
@@ -28,92 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.chartBlocking = new LiveCharts.WinForms.CartesianChart();
-            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.tsClose = new System.Windows.Forms.ToolStripButton();
-            this.tsUp = new System.Windows.Forms.ToolStripButton();
-            this.lblBlocking = new System.Windows.Forms.ToolStripLabel();
-            this.toolStrip1.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // chartBlocking
-            // 
-            this.chartBlocking.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.chartBlocking.Location = new System.Drawing.Point(0, 0);
-            this.chartBlocking.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.chartBlocking.Name = "chartBlocking";
-            this.chartBlocking.Size = new System.Drawing.Size(712, 510);
-            this.chartBlocking.TabIndex = 0;
-            this.chartBlocking.Text = "cartesianChart1";
+            LiveChartsCore.SkiaSharpView.SKCharts.SKDefaultLegend skDefaultLegend1 = new LiveChartsCore.SkiaSharpView.SKCharts.SKDefaultLegend();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Blocking));
+            LiveChartsCore.Drawing.Padding padding1 = new LiveChartsCore.Drawing.Padding();
+            LiveChartsCore.SkiaSharpView.SKCharts.SKDefaultTooltip skDefaultTooltip1 = new LiveChartsCore.SkiaSharpView.SKCharts.SKDefaultTooltip();
+            LiveChartsCore.Drawing.Padding padding2 = new LiveChartsCore.Drawing.Padding();
+            toolStrip1 = new System.Windows.Forms.ToolStrip();
+            tsClose = new System.Windows.Forms.ToolStripButton();
+            tsUp = new System.Windows.Forms.ToolStripButton();
+            lblBlocking = new System.Windows.Forms.ToolStripLabel();
+            chartBlocking = new LiveChartsCore.SkiaSharpView.WinForms.CartesianChart();
+            toolStrip1.SuspendLayout();
+            SuspendLayout();
             // 
             // toolStrip1
             // 
-            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.tsClose,
-            this.tsUp,
-            this.lblBlocking});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
-            this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(712, 27);
-            this.toolStrip1.TabIndex = 1;
-            this.toolStrip1.Text = "toolStrip1";
+            toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { tsClose, tsUp, lblBlocking });
+            toolStrip1.Location = new System.Drawing.Point(0, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new System.Drawing.Size(712, 25);
+            toolStrip1.TabIndex = 1;
+            toolStrip1.Text = "toolStrip1";
             // 
             // tsClose
             // 
-            this.tsClose.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
-            this.tsClose.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.tsClose.Image = global::DBADashGUI.Properties.Resources.Close_red_16x;
-            this.tsClose.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.tsClose.Name = "tsClose";
-            this.tsClose.Size = new System.Drawing.Size(29, 24);
-            this.tsClose.Text = "Close";
-            this.tsClose.Visible = false;
-            this.tsClose.Click += new System.EventHandler(this.TsClose_Click);
+            tsClose.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            tsClose.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            tsClose.Image = Properties.Resources.Close_red_16x;
+            tsClose.ImageTransparentColor = System.Drawing.Color.Magenta;
+            tsClose.Name = "tsClose";
+            tsClose.Size = new System.Drawing.Size(29, 24);
+            tsClose.Text = "Close";
+            tsClose.Visible = false;
+            tsClose.Click += TsClose_Click;
             // 
             // tsUp
             // 
-            this.tsUp.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
-            this.tsUp.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.tsUp.Image = global::DBADashGUI.Properties.Resources.arrow_Up_16xLG;
-            this.tsUp.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.tsUp.Name = "tsUp";
-            this.tsUp.Size = new System.Drawing.Size(29, 24);
-            this.tsUp.Text = "Move Up";
-            this.tsUp.Visible = false;
-            this.tsUp.Click += new System.EventHandler(this.TsUp_Click);
+            tsUp.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            tsUp.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            tsUp.Image = Properties.Resources.arrow_Up_16xLG;
+            tsUp.ImageTransparentColor = System.Drawing.Color.Magenta;
+            tsUp.Name = "tsUp";
+            tsUp.Size = new System.Drawing.Size(29, 24);
+            tsUp.Text = "Move Up";
+            tsUp.Visible = false;
+            tsUp.Click += TsUp_Click;
             // 
             // lblBlocking
             // 
-            this.lblBlocking.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
-            this.lblBlocking.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.lblBlocking.Name = "lblBlocking";
-            this.lblBlocking.Size = new System.Drawing.Size(69, 24);
-            this.lblBlocking.Text = "Blocking";
+            lblBlocking.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            lblBlocking.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            lblBlocking.Name = "lblBlocking";
+            lblBlocking.Size = new System.Drawing.Size(69, 22);
+            lblBlocking.Text = "Blocking";
+            // 
+            // cartesianChart1
+            // 
+            chartBlocking.AutoUpdateEnabled = true;
+            chartBlocking.ChartTheme = null;
+            chartBlocking.Dock = System.Windows.Forms.DockStyle.Fill;
+            chartBlocking.ForceGPU = false;
+            skDefaultLegend1.AnimationsSpeed = System.TimeSpan.Parse("00:00:00.1500000");
+            skDefaultLegend1.Content = null;
+            skDefaultLegend1.IsValid = true;
+            skDefaultLegend1.Opacity = 1F;
+            padding1.Bottom = 0F;
+            padding1.Left = 0F;
+            padding1.Right = 0F;
+            padding1.Top = 0F;
+            skDefaultLegend1.Padding = padding1;
+            skDefaultLegend1.RemoveOnCompleted = false;
+            skDefaultLegend1.RotateTransform = 0F;
+            skDefaultLegend1.X = 0F;
+            skDefaultLegend1.Y = 0F;
+            chartBlocking.Legend = skDefaultLegend1;
+            chartBlocking.Location = new System.Drawing.Point(0, 25);
+            chartBlocking.MatchAxesScreenDataRatio = false;
+            chartBlocking.Name = "cartesianChart1";
+            chartBlocking.Size = new System.Drawing.Size(712, 485);
+            chartBlocking.TabIndex = 2;
+            skDefaultTooltip1.AnimationsSpeed = System.TimeSpan.Parse("00:00:00.1500000");
+            skDefaultTooltip1.Content = null;
+            skDefaultTooltip1.IsValid = true;
+            skDefaultTooltip1.Opacity = 1F;
+            padding2.Bottom = 0F;
+            padding2.Left = 0F;
+            padding2.Right = 0F;
+            padding2.Top = 0F;
+            skDefaultTooltip1.Padding = padding2;
+            skDefaultTooltip1.RemoveOnCompleted = false;
+            skDefaultTooltip1.RotateTransform = 0F;
+            skDefaultTooltip1.Wedge = 10;
+            skDefaultTooltip1.X = 0F;
+            skDefaultTooltip1.Y = 0F;
+            chartBlocking.Tooltip = skDefaultTooltip1;
+            chartBlocking.TooltipFindingStrategy = LiveChartsCore.Measure.TooltipFindingStrategy.Automatic;
+            chartBlocking.UpdaterThrottler = System.TimeSpan.Parse("00:00:00.0500000");
             // 
             // Blocking
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.toolStrip1);
-            this.Controls.Add(this.chartBlocking);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.Name = "Blocking";
-            this.Size = new System.Drawing.Size(712, 510);
-            this.Load += new System.EventHandler(this.Blocking_Load);
-            this.toolStrip1.ResumeLayout(false);
-            this.toolStrip1.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            Controls.Add(chartBlocking);
+            Controls.Add(toolStrip1);
+            Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            Name = "Blocking";
+            Size = new System.Drawing.Size(712, 510);
+            Load += Blocking_Load;
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
 
         }
 
         #endregion
-
-        private LiveCharts.WinForms.CartesianChart chartBlocking;
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.ToolStripLabel lblBlocking;
         private System.Windows.Forms.ToolStripButton tsClose;
         private System.Windows.Forms.ToolStripButton tsUp;
+        private LiveChartsCore.SkiaSharpView.WinForms.CartesianChart chartBlocking;
     }
 }
